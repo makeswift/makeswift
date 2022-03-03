4df5165ce4cccdd658c15e7f6ded0c7de26ea75d
Fikri Karim <fkfikrikarim@gmail.com>
2022-03-03 21:28:59 +0700

    feat: add SocialLinks component

---
+++ packages/runtime/src/components/SocialLinks/options.tsx
@@ -0,0 +1,47 @@
+import LogoAngellist20 from '../../icons/logo-angellist-20.svg'
+import LogoCodepen20 from '../../icons/logo-codepen-20.svg'
+import LogoDribbble20 from '../../icons/logo-dribbble-20.svg'
+import LogoFacebook20 from '../../icons/logo-facebook-20.svg'
+import LogoGithub20 from '../../icons/logo-github-20.svg'
+import LogoInstagram20 from '../../icons/logo-instagram-20.svg'
+import LogoLinkedin20 from '../../icons/logo-linkedin-20.svg'
+import LogoMedium20 from '../../icons/logo-medium-20.svg'
+import LogoPinterest20 from '../../icons/logo-pinterest-20.svg'
+import LogoReddit20 from '../../icons/logo-reddit-20.svg'
+import LogoRss20 from '../../icons/logo-rss-20.svg'
+import LogoSnapchat20 from '../../icons/logo-snapchat-20.svg'
+import LogoSoundcloud20 from '../../icons/logo-soundcloud-20.svg'
+import LogoSpotify20 from '../../icons/logo-spotify-20.svg'
+import LogoTelegram20 from '../../icons/logo-telegram-20.svg'
+import LogoTumblr20 from '../../icons/logo-tumblr-20.svg'
+import LogoTwitch20 from '../../icons/logo-twitch-20.svg'
+import LogoTwitter20 from '../../icons/logo-twitter-20.svg'
+import LogoWhatsapp20 from '../../icons/logo-whatsapp-20.svg'
+import LogoVimeo20 from '../../icons/logo-vimeo-20.svg'
+import LogoYelp20 from '../../icons/logo-yelp-20.svg'
+import LogoYoutube20 from '../../icons/logo-youtube-20.svg'
+
+export const SocialLinksOptions = [
+  { type: 'angellist', label: 'AngelList', icon: <LogoAngellist20 />, brandColor: 'black' },
+  { type: 'codepen', label: 'CodePen', icon: <LogoCodepen20 />, brandColor: 'black' },
+  { type: 'dribbble', label: 'Dribbble', icon: <LogoDribbble20 />, brandColor: '#ea4c89' },
+  { type: 'facebook', label: 'Facebook', icon: <LogoFacebook20 />, brandColor: '#3b5998' },
+  { type: 'github', label: 'GitHub', icon: <LogoGithub20 />, brandColor: 'black' },
+  { type: 'instagram', label: 'Instagram', icon: <LogoInstagram20 />, brandColor: 'black' },
+  { type: 'linkedin', label: 'LinkedIn', icon: <LogoLinkedin20 />, brandColor: '#0077b5' },
+  { type: 'medium', label: 'Medium', icon: <LogoMedium20 />, brandColor: 'black' },
+  { type: 'pinterest', label: 'Pinterest', icon: <LogoPinterest20 />, brandColor: '#bd081c' },
+  { type: 'reddit', label: 'Reddit', icon: <LogoReddit20 />, brandColor: '#ff4500' },
+  { type: 'rss', label: 'RSS', icon: <LogoRss20 />, brandColor: 'black' },
+  { type: 'snapchat', label: 'Snapchat', icon: <LogoSnapchat20 />, brandColor: '#fffc00' },
+  { type: 'soundcloud', label: 'SoundCloud', icon: <LogoSoundcloud20 />, brandColor: '#ff3300' },
+  { type: 'spotify', label: 'Spotify', icon: <LogoSpotify20 />, brandColor: '#1db954' },
+  { type: 'telegram', label: 'Telegram', icon: <LogoTelegram20 />, brandColor: '#0088cc' },
+  { type: 'tumblr', label: 'Tumblr', icon: <LogoTumblr20 />, brandColor: '#35465c' },
+  { type: 'twitch', label: 'Twitch', icon: <LogoTwitch20 />, brandColor: '#6441A4' },
+  { type: 'twitter', label: 'Twitter', icon: <LogoTwitter20 />, brandColor: '#1da1f2' },
+  { type: 'whatsapp', label: 'WhatsApp', icon: <LogoWhatsapp20 />, brandColor: '#25d366' },
+  { type: 'vimeo', label: 'Vimeo', icon: <LogoVimeo20 />, brandColor: '#1ab7ea' },
+  { type: 'yelp', label: 'Yelp', icon: <LogoYelp20 />, brandColor: '#af0606' },
+  { type: 'youtube', label: 'YouTube', icon: <LogoYoutube20 />, brandColor: '#ff0000' },
+] as const
