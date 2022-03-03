8369a7fa46d3fdaf861c9877448b4a0e431a9e07
Fikri Karim <fkfikrikarim@gmail.com>
2022-03-04 00:25:29 +0700

    fix: move components into builtin and shared

---
+++ packages/runtime/src/components/builtin/SocialLinks/options.tsx
@@ -0,0 +1,47 @@
+import { ReactComponent as LogoAngellist20 } from '../../icons/logo-angellist-20.svg'
+import { ReactComponent as LogoCodepen20 } from '../../icons/logo-codepen-20.svg'
+import { ReactComponent as LogoDribbble20 } from '../../icons/logo-dribbble-20.svg'
+import { ReactComponent as LogoFacebook20 } from '../../icons/logo-facebook-20.svg'
+import { ReactComponent as LogoGithub20 } from '../../icons/logo-github-20.svg'
+import { ReactComponent as LogoInstagram20 } from '../../icons/logo-instagram-20.svg'
+import { ReactComponent as LogoLinkedin20 } from '../../icons/logo-linkedin-20.svg'
+import { ReactComponent as LogoMedium20 } from '../../icons/logo-medium-20.svg'
+import { ReactComponent as LogoPinterest20 } from '../../icons/logo-pinterest-20.svg'
+import { ReactComponent as LogoReddit20 } from '../../icons/logo-reddit-20.svg'
+import { ReactComponent as LogoRss20 } from '../../icons/logo-rss-20.svg'
+import { ReactComponent as LogoSnapchat20 } from '../../icons/logo-snapchat-20.svg'
+import { ReactComponent as LogoSoundcloud20 } from '../../icons/logo-soundcloud-20.svg'
+import { ReactComponent as LogoSpotify20 } from '../../icons/logo-spotify-20.svg'
+import { ReactComponent as LogoTelegram20 } from '../../icons/logo-telegram-20.svg'
+import { ReactComponent as LogoTumblr20 } from '../../icons/logo-tumblr-20.svg'
+import { ReactComponent as LogoTwitch20 } from '../../icons/logo-twitch-20.svg'
+import { ReactComponent as LogoTwitter20 } from '../../icons/logo-twitter-20.svg'
+import { ReactComponent as LogoWhatsapp20 } from '../../icons/logo-whatsapp-20.svg'
+import { ReactComponent as LogoVimeo20 } from '../../icons/logo-vimeo-20.svg'
+import { ReactComponent as LogoYelp20 } from '../../icons/logo-yelp-20.svg'
+import { ReactComponent as LogoYoutube20 } from '../../icons/logo-youtube-20.svg'
+
+export const SocialLinksOptions = [
+  { type: 'angellist', label: 'AngelList', icon: <LogoAngellist20 />, brandColor: 'black' },
+  { type: 'codepen', label: 'CodePen', icon: <LogoCodepen20 />, brandColor: 'black' },
+  { type: 'dribbble', label: 'Dribbble', icon: <LogoDribbble20 />, brandColor: '#ea4c89' },
+  { type: 'facebook', label: 'Facebook', icon: <LogoFacebook20 />, brandColor: '#3b5998' },
+  { type: 'github', label: 'GitHub', icon: <LogoGithub20 />, brandColor: 'black' },
+  { type: 'instagram', label: 'Instagram', icon: <LogoInstagram20 />, brandColor: 'black' },
+  { type: 'linkedin', label: 'LinkedIn', icon: <LogoLinkedin20 />, brandColor: '#0077b5' },
+  { type: 'medium', label: 'Medium', icon: <LogoMedium20 />, brandColor: 'black' },
+  { type: 'pinterest', label: 'Pinterest', icon: <LogoPinterest20 />, brandColor: '#bd081c' },
+  { type: 'reddit', label: 'Reddit', icon: <LogoReddit20 />, brandColor: '#ff4500' },
+  { type: 'rss', label: 'RSS', icon: <LogoRss20 />, brandColor: 'black' },
+  { type: 'snapchat', label: 'Snapchat', icon: <LogoSnapchat20 />, brandColor: '#fffc00' },
+  { type: 'soundcloud', label: 'SoundCloud', icon: <LogoSoundcloud20 />, brandColor: '#ff3300' },
+  { type: 'spotify', label: 'Spotify', icon: <LogoSpotify20 />, brandColor: '#1db954' },
+  { type: 'telegram', label: 'Telegram', icon: <LogoTelegram20 />, brandColor: '#0088cc' },
+  { type: 'tumblr', label: 'Tumblr', icon: <LogoTumblr20 />, brandColor: '#35465c' },
+  { type: 'twitch', label: 'Twitch', icon: <LogoTwitch20 />, brandColor: '#6441A4' },
+  { type: 'twitter', label: 'Twitter', icon: <LogoTwitter20 />, brandColor: '#1da1f2' },
+  { type: 'whatsapp', label: 'WhatsApp', icon: <LogoWhatsapp20 />, brandColor: '#25d366' },
+  { type: 'vimeo', label: 'Vimeo', icon: <LogoVimeo20 />, brandColor: '#1ab7ea' },
+  { type: 'yelp', label: 'Yelp', icon: <LogoYelp20 />, brandColor: '#af0606' },
+  { type: 'youtube', label: 'YouTube', icon: <LogoYoutube20 />, brandColor: '#ff0000' },
+] as const
---
+++ packages/runtime/src/components/shared/Link/index.tsx
@@ -0,0 +1,114 @@
+import { ComponentPropsWithoutRef, forwardRef, MouseEvent } from 'react'
+import scrollIntoView from 'scroll-into-view-if-needed'
+import NextLink from 'next/link'
+
+import { LinkValue as LinkPropControllerValue } from '../../../prop-controllers/descriptors'
+import { usePage } from '../../hooks'
+import { useElementId } from '../../../runtimes/react'
+
+type BaseProps = {
+  link?: LinkPropControllerValue
+  onClick?: (event: MouseEvent<HTMLAnchorElement>) => unknown
+}
+
+type Props = BaseProps & Omit<ComponentPropsWithoutRef<'a'>, keyof BaseProps>
+
+export const Link = forwardRef<HTMLAnchorElement, Props>(function Link(
+  { link, onClick = () => {}, ...restOfProps }: Props,
+  ref,
+) {
+  const page = usePage(link && link.type === 'OPEN_PAGE' ? link.payload.pageId : null)
+  const elementKey =
+    link?.type === 'SCROLL_TO_ELEMENT' ? link.payload.elementIdConfig?.elementKey : null
+  const elementId = useElementId(elementKey)
+
+  let href = ''
+  let target: '_blank' | '_self' | undefined
+  let block: 'start' | 'center' | 'end' | undefined
+
+  if (link) {
+    switch (link.type) {
+      case 'OPEN_PAGE': {
+        if (page) href = `/${page.pathname}`
+
+        target = link.payload.openInNewTab ? '_blank' : '_self'
+
+        break
+      }
+
+      case 'OPEN_URL': {
+        href = link.payload.url
+
+        target = link.payload.openInNewTab ? '_blank' : '_self'
+
+        break
+      }
+
+      case 'SEND_EMAIL': {
+        const { to, subject = '', body = '' } = link.payload
+
+        if (to != null) href = `mailto:${to}?subject=${subject}&body=${body}`
+
+        break
+      }
+
+      case 'CALL_PHONE': {
+        href = `tel:${link.payload.phoneNumber}`
+
+        break
+      }
+
+      case 'SCROLL_TO_ELEMENT': {
+        href = `#${elementId ?? ''}`
+        block = link.payload.block
+
+        break
+      }
+
+      default:
+        throw new RangeError(`Invalid link type "${(link as any).type}."`)
+    }
+  }
+
+  function handleClick(event: MouseEvent<HTMLAnchorElement>) {
+    onClick(event)
+
+    if (event.defaultPrevented) return
+
+    /**
+     * When we introduced `next/link` instead of just `a` element slate no longer prevented link from navigating within
+     * content mode. This is a hack to compensate for what would be expected as slate's default behavior.
+     * On upgrade of slate this can be reevaluated.
+     */
+    if (event.currentTarget.isContentEditable) return event.preventDefault()
+
+    if (link && link.type === 'SCROLL_TO_ELEMENT') {
+      let hash: string | undefined
+
+      try {
+        hash = new URL(`http://www.example.com/${href}`).hash
+      } catch (error) {
+        console.error(`Link received invalid href: ${href}`, error)
+      }
+
+      if (href != null && href === hash) {
+        event.preventDefault()
+        const view = event.view as unknown as Window
+
+        scrollIntoView(view.document.querySelector(hash)!, {
+          behavior: 'smooth',
+          block,
+        })
+
+        if (view.location.hash !== hash) view.history.pushState({}, '', hash)
+      }
+    }
+  }
+
+  return (
+    <NextLink href={href}>
+      {/* eslint-disable-next-line */}
+      <a {...restOfProps} ref={ref} target={target} onClick={handleClick} />
+    </NextLink>
+  )
+})
