f28028b0d4da07a91536664df412d9c6ae70c4c6
Fikri Karim <fkfikrikarim@gmail.com>
2022-03-03 21:28:53 +0700

    feat: add svgr plugin

--- packages/runtime/vite.config.ts
+++ packages/runtime/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from '@svgr/rollup'
 import * as path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr(), react()],
   build: {
     emptyOutDir: false,
     sourcemap: true,
