972247c8462d80ba3ddd5136848a71d16cd02f6e
Fikri Karim <fkfikrikarim@gmail.com>
2022-04-08 15:53:09 +0700

    feat: add Page (fonts, metadata, snippets)

--- packages/runtime/src/components/index.ts
+++ packages/runtime/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './builtin'
 export * from './utils/cssMediaRules'
 export * from './hooks'
+export * from './page'
---
+++ packages/runtime/src/components/page/BodySnippet.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from 'react'
+
+const SCRIPT_TAG = 'script'
+
+type Props = {
+  code: string
+  cleanup: string | null | undefined
+}
+
+export function BodySnippet({ code, cleanup }: Props): null {
+  useEffect(() => {
+    const container = document.createElement('div')
+
+    container.innerHTML = code
+
+    const walker = document.createTreeWalker(container, NodeFilter.SHOW_ELEMENT)
+    const scripts: HTMLScriptElement[] = []
+
+    while (walker.nextNode()) {
+      if (walker.currentNode instanceof HTMLScriptElement) scripts.push(walker.currentNode)
+    }
+
+    scripts.forEach(inlineScript => {
+      const executableScript = document.createElement(SCRIPT_TAG)
+
+      executableScript.textContent = inlineScript.textContent
+      Array.from(inlineScript.attributes).forEach(({ name, value }) => {
+        executableScript.setAttribute(name, value)
+      })
+
+      inlineScript.parentNode?.replaceChild(executableScript, inlineScript)
+    })
+
+    const nodes = Array.from(container.childNodes)
+
+    document.body.append(...nodes)
+
+    return () => {
+      nodes.forEach(node => {
+        node.parentNode?.removeChild(node)
+      })
+
+      if (cleanup == null) return
+
+      const cleanUp = new Function(cleanup)
+
+      try {
+        cleanUp()
+      } catch {
+        // Ignore errors from user input.
+      }
+    }
+  }, [code, cleanup])
+
+  return null
+}
