0e269713758cbd54a7c5d351352313ce41b650aa
Miguel Oller <miguel@makeswift.com>
2022-07-12 17:36:25 -0400

    feat: add @makeswift/next-plugin

---
+++ .changeset/giant-colts-cough.md
@@ -0,0 +1,7 @@
+---
+"@makeswift/next-plugin": minor
+---
+
+Release @makeswift/next-plugin.
+
+This plugin automatically configures the Makeswift domain for next/image and also enables code-splitting via next/dynamic.
---
+++ packages/makeswift-next-plugin/index.js
@@ -0,0 +1,13 @@
+const withTmInitializer = require('next-transpile-modules')
+
+const NEXT_IMAGE_DOMAINS = ['s.mkswft.com']
+const NEXT_TRANSPILE_MODULES_MODULES = ['@makeswift/runtime']
+
+module.exports = ({ resolveSymlinks } = {}) => (nextConfig = {}) =>
+  withTmInitializer(NEXT_TRANSPILE_MODULES_MODULES, { resolveSymlinks })({
+    ...nextConfig,
+    images: {
+      ...nextConfig.images,
+      domains: [...(nextConfig.images?.domains ?? []), ...NEXT_IMAGE_DOMAINS],
+    },
+  })
