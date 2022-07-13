ddbc14fa1843f26c257174905e967ca25bb1b8c5
Miguel Oller <miguel@makeswift.com>
2022-07-13 15:04:11 -0400

    perf: code-splitting for Navigation component

--- packages/runtime/src/components/builtin/Navigation/Navigation.tsx
+++ packages/runtime/src/components/builtin/Navigation/Navigation.tsx
@@ -29,10 +29,7 @@ import {
 import { cssMargin, cssMediaRules, cssTextStyle } from '../../utils/cssMediaRules'
 import { ColorValue as Color } from '../../utils/types'
 import { colorToString } from '../../utils/colorToString'
-import { ReactRuntime } from '../../../react'
-import { Props } from '../../../prop-controllers'
 import { ResponsiveColor } from '../../../runtimes/react/controls'
-import { findDeviceOverride } from '../../utils/devices'
 import { useResponsiveColor } from '../../hooks'
 
 type Props = {
@@ -224,104 +221,3 @@ const Navigation = forwardRef<HTMLDivElement, Props>(function Navigation(
 })
 
 export default Navigation
-
-export function registerComponent(runtime: ReactRuntime) {
-  return runtime.registerComponent(Navigation, {
-    type: './components/Navigation/index.js',
-    label: 'Navigation',
-    icon: 'Navigation40',
-    props: {
-      id: Props.ElementID(),
-      links: Props.NavigationLinks(),
-      linkTextStyle: Props.TextStyle(props => {
-        const links = props.links as NavigationLinksValue
-
-        return {
-          label: 'Link text style',
-          hidden: links == null || links.length === 0,
-        }
-      }),
-      showLogo: Props.Checkbox({ preset: true, label: 'Show logo' }),
-      logoFile: Props.Image(props => ({
-        label: 'Logo',
-        hidden: props.showLogo === false,
-      })),
-      logoWidth: Props.ResponsiveLength(props => ({
-        preset: [{ deviceId: 'desktop', value: { value: 100, unit: 'px' } }],
-        label: 'Logo width',
-        min: 0,
-        max: 1000,
-        // TODO: This is hardcoded value, import it from LengthInputOptions
-        options: [{ value: 'px', label: 'Pixels', icon: 'Px16' }],
-        hidden: props.showLogo === false,
-      })),
-      logoAltText: Props.TextInput(props => ({
-        label: 'Logo alt text',
-        hidden: props.showLogo === false,
-      })),
-      logoLink: Props.Link(props => ({
-        label: 'Logo on click',
-        hidden: props.showLogo === false,
-      })),
-      alignment: Props.ResponsiveIconRadioGroup({
-        label: 'Alignment',
-        options: [
-          { label: 'Left', value: 'flex-start', icon: 'AlignLeft16' },
-          { label: 'Center', value: 'center', icon: 'AlignCenter16' },
-          { label: 'End', value: 'flex-end', icon: 'AlignRight16' },
-        ],
-        defaultValue: 'flex-end',
-      }),
-      gutter: Props.GapX({
-        preset: [{ deviceId: 'desktop', value: { value: 10, unit: 'px' } }],
-        label: 'Link gap',
-        min: 0,
-        max: 100,
-        step: 1,
-        defaultValue: { value: 0, unit: 'px' },
-      }),
-      mobileMenuAnimation: Props.ResponsiveSelect({
-        label: 'Mobile menu',
-        options: [
-          { value: 'coverRight', label: 'Cover from right' },
-          { value: 'coverLeft', label: 'Cover from left' },
-        ],
-      }),
-      mobileMenuOpenIconColor: Props.ResponsiveColor((props, device) => {
-        const mobileMenuAnimation = props.mobileMenuAnimation as ResponsiveValue<string>
-        const hidden = !findDeviceOverride(mobileMenuAnimation, device)
-
-        return {
-          label: 'Open icon color',
-          placeholder: 'rgba(161, 168, 194, 0.5)',
-          hidden,
-        }
-      }),
-      mobileMenuCloseIconColor: Props.ResponsiveColor((props, device) => {
-        const mobileMenuAnimation = props.mobileMenuAnimation as ResponsiveValue<string>
-        const hidden = !findDeviceOverride(mobileMenuAnimation, device)
-
-        return {
-          label: 'Close icon color',
-          placeholder: 'rgba(161, 168, 194, 0.5)',
-          hidden,
-        }
-      }),
-      mobileMenuBackgroundColor: Props.ResponsiveColor((props, device) => {
-        const mobileMenuAnimation = props.mobileMenuAnimation as ResponsiveValue<string>
-        const hidden = !findDeviceOverride(mobileMenuAnimation, device)
-
-        return {
-          label: 'Menu BG color',
-          placeholder: 'black',
-          hidden,
-        }
-      }),
-      width: Props.Width({
-        format: Props.Width.Formats.ClassName,
-        defaultValue: { value: 100, unit: '%' },
-      }),
-      margin: Props.Margin(),
-    },
-  })
-}
