02e3a0ef5597756c864bf2b29b232887d41a8856
Miguel Oller <miguel@makeswift.com>
2022-07-27 16:42:02 -0400

    feat: use GridItem in Box builtin component

--- packages/runtime/src/components/utils/responsive-style.ts
+++ packages/runtime/src/components/utils/responsive-style.ts
@@ -96,7 +96,6 @@ export function responsiveGridItem(props: {
 }): CSSObject {
   return {
     display: 'flex',
-    alignItems: 'flex-start',
     ...responsiveStyle(
       [props.grid, props.columnGap, props.rowGap] as const,
       ([
