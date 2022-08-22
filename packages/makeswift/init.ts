bc194d7baebda501e2d0dcf93a257d2f40b8c7b2
Alex Woods <alexhwoods@gmail.com>
2022-08-22 14:25:04 -0400

    feat: complete handshake

--- packages/makeswift/init.ts
+++ packages/makeswift/init.ts
@@ -1,12 +1,92 @@
-import { exec } from "child_process";
 import { createNextApp } from "./create-next-app";
+import spawn from "cross-spawn";
 import * as path from "path";
+import * as http from "http";
+import open from "open";
+import * as fs from "fs";
+import detectCb from "detect-port";
+import { promisify } from "util";
+
+const detect = promisify(detectCb);
+
+const MAKESWIFT_APP = `http://localhost:7000`;
+const siteSelectionPath = "select-site";
 
 async function init(
   name: string,
   { example = "basic-typescript" }: { example?: string }
 ): Promise<void> {
-  await createNextApp({ dir: path.join(__dirname, name), example });
+  const nextAppDir = path.join(__dirname, name);
+  createNextApp({
+    dir: nextAppDir,
+    example,
+  });
+
+  const callbackUrl = `http://localhost:5005/${siteSelectionPath}`;
+  // Handshake Step 1
+  await open(
+    `${MAKESWIFT_APP}/cli/select-site?project_name=${name}&callback_url=${callbackUrl}`
+  );
+
+  // Handshake Step 2 - the browser goes to `callbackUrl`
+  const nextAppPort = await detect(3000);
+  const nextAppUrl = `http://localhost:${nextAppPort}`;
+  const redirectUrl = `${MAKESWIFT_APP}/link-site&host_url=${nextAppUrl}`;
+
+  // Handshake Step 3 - we redirect the browser to redirectUrl
+  const { siteApiKey } = await getSiteApiKey({ port: 5005, redirectUrl });
+
+  // In the background, we're setting up the Next app with the API key
+  // and starting the app at `nextAppPort`
+  const envLocal = `MAKESWIFT_SITE_API_KEY=${siteApiKey}\n`;
+  fs.writeFileSync(`${nextAppDir}/.env.local`, envLocal);
+
+  spawn.sync("yarn", ["dev", "--port", nextAppPort.toString()], {
+    stdio: "inherit",
+    cwd: nextAppDir,
+  });
+
+  // Handshake Step 4 - Leo redirects to the builder with the site open,
+  //                    with the host using `nextAppUrl` for the builder
+}
+
+async function getSiteApiKey({
+  port,
+  redirectUrl,
+}: {
+  port: number;
+  redirectUrl: string;
+}): Promise<{ siteApiKey: string }> {
+  return new Promise<{ siteApiKey: string }>((resolve, reject) => {
+    const server = http
+      .createServer((req, res) => {
+        const url = new URL(req.url!, `http://${req.headers.host}`);
+        if (url.pathname !== `/${siteSelectionPath}`) {
+          reject(new Error("The CLI does not know how to handle that path."));
+          return;
+        }
+
+        const queryParams = url.searchParams;
+        const siteApiKey = queryParams.get("site_api_key");
+        if (!siteApiKey) {
+          reject(
+            new Error("Select site redirect URL does not contain Site API Key.")
+          );
+          return;
+        }
+
+        res.writeHead(302, {
+          Location: redirectUrl,
+        });
+        res.end();
+        server.close();
+
+        resolve({
+          siteApiKey,
+        });
+      })
+      .listen(port);
+  });
 }
 
 export default init;
