07d772c1c5411ecc1ac31f6a95cd29be1772c548
Alex Woods <alexhwoods@gmail.com>
2022-08-23 13:42:32 -0400

    fix: route is /makeswift

--- packages/makeswift/init.ts
+++ packages/makeswift/init.ts
@@ -32,7 +32,7 @@ async function init(
   await open(selectSiteUrl.toString());
 
   // Handshake Step 2 - the browser goes to `callbackUrl`
-  const nextAppUrl = `http://localhost:${nextAppPort}`;
+  const nextAppUrl = `http://localhost:${nextAppPort}/makeswift`;
   const redirectUrl = new URL(`${MAKESWIFT_APP}/cli/link-site`);
   redirectUrl.searchParams.set("host_url", nextAppUrl);
 
