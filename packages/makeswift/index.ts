960057dcaae5fb493014b1fc89dffd362536d813
Alex Woods <alexhwoods@gmail.com>
2022-08-24 11:22:16 -0400

    feat: detect if inside a next application

--- packages/makeswift/index.ts
+++ packages/makeswift/index.ts
@@ -1,33 +1,46 @@
-import init from './init'
+import init from "./init";
+import isNextApp from "./isNextApp";
+import link from "./link";
 
-const { Command } = require('commander')
-const program = new Command()
-
-program
-  .name('Makeswift CLI')
-  .description('The official command-line tool to interact with Makeswift.')
-  .version('0.0.0')
+const { Command } = require("commander");
+const program = new Command();
 
 program
-  .command('hello')
-  .description('A hello world command. Revolutionary.')
-  .argument('<name>', 'The name of the person to say hello to.')
-  .option('--world', 'Say hi to the world, or to the name.')
+  .name("Makeswift CLI")
+  .description("The official command-line tool to interact with Makeswift.")
+  .version("0.0.0");
+
+program
+  .command("hello")
+  .description("A hello world command. Revolutionary.")
+  .argument("<name>", "The name of the person to say hello to.")
+  .option("--world", "Say hi to the world, or to the name.")
   .action((name: string, options: { world?: boolean; separator: string }) => {
     if (options.world == true) {
-      console.log(`Hello world`)
-      return
+      console.log(`Hello world`);
+      return;
     }
 
-    console.log(`name: ${JSON.stringify(name, null, 2)}`)
-    console.log(`Hello ${name}`)
-  })
+    console.log(`name: ${JSON.stringify(name, null, 2)}`);
+    console.log(`Hello ${name}`);
+  });
 
 program
-  .command('init')
-  .description('Create a new Next.js app or integrate an existing one, to use with Makeswift.')
-  .argument('<name>', 'The name of the folder to create.')
-  .option('--example <example>', 'The Github URL of the Makeswift example to clone.')
-  .action(init)
+  .command("init")
+  .description(
+    "Create a new Next.js app or integrate an existing one, to use with Makeswift."
+  )
+  .argument("<name>", "The name of the folder to create.")
+  .option(
+    "--example <example>",
+    "The Github URL of the Makeswift example to clone."
+  )
+  .action(init);
 
-program.parse()
+program
+  .command("link")
+  .description("Link an existing Next.js app to a Makeswift site.")
+  .argument("[name]", "The name of the app.")
+  .action(link);
+
+program.parse();
---
+++ packages/makeswift/link.ts
@@ -0,0 +1,14 @@
+import * as path from "path";
+import isNextApp from "./isNextApp";
+
+async function link(name: string): Promise<void> {
+  const appDirectory = name ? path.join(__dirname, name) : path.join(__dirname);
+
+  if (isNextApp(appDirectory)) {
+    console.log("linking app");
+  } else {
+    console.log("Did not detect a Next.js application.");
+  }
+}
+
+export default link;
