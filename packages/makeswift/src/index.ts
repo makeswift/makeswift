f9bf3e920d5b5fdce27346c827cf1748725c5f04
Alex Woods <alexhwoods@gmail.com>
2022-08-24 16:02:15 -0400

    refactor: allow for publishing

---
+++ packages/makeswift/src/index.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import init from "./init";
+import pkg from "../package.json";
+
+const program = new Command();
+
+program
+  .name("Makeswift CLI")
+  .description("The official command-line tool to interact with Makeswift.")
+  .version(pkg.version);
+
+program
+  .command("init")
+  .description(
+    "Create a new Next.js app or integrate an existing one, to use with Makeswift."
+  )
+  .argument("<name>", "The name of the folder to create.")
+  .option(
+    "--example <example>",
+    "The Github URL, or name of the Makeswift example to clone."
+  )
+  .action(init);
+
+program.parse();
