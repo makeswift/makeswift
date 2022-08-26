a3b24f01485e439615cc971fc2ce9381b04b1b6b
Alex Woods <alexhwoods@gmail.com>
2022-08-26 14:48:08 -0400

    feat: set up prettier

--- packages/makeswift/isNextApp.ts
+++ packages/makeswift/isNextApp.ts
@@ -1,28 +1,28 @@
-import path from "path";
-import * as fs from "fs";
+import path from 'path'
+import * as fs from 'fs'
 
 function isNextApp(appDirectory: string): boolean {
-  return hasNextDependency(appDirectory) && hasPagesFolder(appDirectory);
+  return hasNextDependency(appDirectory) && hasPagesFolder(appDirectory)
 }
 
 function hasNextDependency(appDirectory: string): boolean {
   try {
-    const packageJson = require(path.join(appDirectory, "package.json"));
-    const { dependencies } = packageJson;
+    const packageJson = require(path.join(appDirectory, 'package.json'))
+    const { dependencies } = packageJson
 
-    return "next" in dependencies;
+    return 'next' in dependencies
   } catch (error) {
-    console.log(`error: ${JSON.stringify(error.message, null, 2)}`);
+    console.log(`error: ${JSON.stringify(error.message, null, 2)}`)
   }
 
-  return false;
+  return false
 }
 
 function hasPagesFolder(appDirectory: string): boolean {
   return (
-    fs.existsSync(path.join(appDirectory, "pages")) ||
-    fs.existsSync(path.join(appDirectory, "src/pages"))
-  );
+    fs.existsSync(path.join(appDirectory, 'pages')) ||
+    fs.existsSync(path.join(appDirectory, 'src/pages'))
+  )
 }
 
-export default isNextApp;
+export default isNextApp
--- packages/makeswift/link.ts
+++ packages/makeswift/link.ts
@@ -1,14 +1,14 @@
-import * as path from "path";
-import isNextApp from "./isNextApp";
+import * as path from 'path'
+import isNextApp from './isNextApp'
 
 async function link(name: string): Promise<void> {
-  const appDirectory = name ? path.join(__dirname, name) : path.join(__dirname);
+  const appDirectory = name ? path.join(__dirname, name) : path.join(__dirname)
 
   if (isNextApp(appDirectory)) {
-    console.log("linking app");
+    console.log('linking app')
   } else {
-    console.log("Did not detect a Next.js application.");
+    console.log('Did not detect a Next.js application.')
   }
 }
 
-export default link;
+export default link
