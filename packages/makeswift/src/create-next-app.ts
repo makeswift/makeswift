36ac511013b3dc37b9c2e612275db60b129dd0c5
Alex Woods <alexhwoods@gmail.com>
2022-08-30 17:57:29 -0400

    refactor: run Prettier

--- packages/makeswift/src/create-next-app.ts
+++ packages/makeswift/src/create-next-app.ts
@@ -1,17 +1,11 @@
-import spawn from "cross-spawn";
-import { coerceExampleToUrl } from "./utils";
+import spawn from 'cross-spawn'
+import { coerceExampleToUrl } from './utils/coerce-example-to-URL'
 
-export function createNextApp({
-  dir,
-  example,
-}: {
-  dir: string;
-  example: string;
-}): void {
-  const url = coerceExampleToUrl(example);
+export function createNextApp({ dir, example }: { dir: string; example: string }): void {
+  const url = coerceExampleToUrl(example)
 
-  spawn.sync("npx", ["--yes", "create-next-app", "--example", url, dir], {
-    stdio: "inherit",
+  spawn.sync('npx', ['--yes', 'create-next-app', '--example', url, dir], {
+    stdio: 'inherit',
     cwd: __dirname,
-  });
+  })
 }
