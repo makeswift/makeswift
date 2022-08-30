ad299300450d2e24386ea79dbb3c828b217a59f2
Alex Woods <alexhwoods@gmail.com>
2022-08-30 18:08:27 -0400

    feat: add makeswift pages

--- packages/makeswift/src/integrate-next-app.ts
+++ packages/makeswift/src/integrate-next-app.ts
@@ -1,4 +1,6 @@
 import spawn from 'cross-spawn'
+import * as fs from 'fs'
+import path from 'path'
 import { yarnOrNpm } from './utils/yarn-or-npm'
 
 export function integrateNextApp({ dir }: { dir: string }): void {
@@ -6,6 +8,9 @@ export function integrateNextApp({ dir }: { dir: string }): void {
 
   // Step 1 - install the runtime
   installMakeswiftRuntime({ dir })
+
+  // Step 2 - add Makeswift pages
+  addMakeswiftPages({ dir })
 }
 
 function installMakeswiftRuntime({ dir }: { dir: string }): void {
@@ -21,4 +26,66 @@ function installMakeswiftRuntime({ dir }: { dir: string }): void {
       cwd: dir,
     })
   }
+}
+
+function addMakeswiftPages({ dir }: { dir: string }): void {
+  const pagesFolder = getPagesFolder({ dir })
+  const extension = getExtension({ dir })
+
+  const previewRoute = `export {
+  getServerSideProps,
+  Page as default
+} from '@makeswift/runtime/next'
+`
+  fs.writeFileSync(path.join(pagesFolder, `makeswift.${extension}`), previewRoute)
+
+  // catch all route
+  const catchAllRoute = `export {
+  getStaticProps,
+  getStaticPaths,
+  Page as default
+} from '@makeswift/runtime/next'
+`
+  fs.writeFileSync(path.join(pagesFolder, `[[...path]].${extension}`), catchAllRoute)
+
+  // custom document
+  const customDocument = `export { Document as default } from '@makeswift/runtime/next'`
+  fs.writeFileSync(path.join(pagesFolder, `_document.${extension}`), customDocument)
+}
+
+function getPagesFolder({ dir }: { dir: string }): string {
+  if (fs.existsSync(path.join(dir, 'pages'))) {
+    return path.join(dir, 'pages')
+  }
+
+  if (fs.existsSync(path.join(dir, 'src/pages'))) {
+    return path.join(dir, '/src/pages')
+  }
+
+  throw Error('Cannot find pages directory in Next.js app.')
+}
+
+function getExtension({ dir }: { dir: string }): 'js' | 'ts' {
+  function hasTSConfig(dir: string): boolean {
+    return fs.existsSync(path.join(dir, 'tsconfig.json'))
+  }
+
+  function hasTSDependency(dir: string): boolean {
+    try {
+      const packageJson = require(path.join(dir, 'package.json'))
+      const { dependencies, devDependencies } = packageJson
+
+      return 'typescript' in dependencies || 'typescript' in devDependencies
+    } catch (error) {
+      if (error instanceof Error) console.log(error.message)
+    }
+
+    return false
+  }
+
+  if (hasTSConfig(dir) || hasTSDependency(dir)) {
+    return 'ts'
+  }
+
+  return 'js'
 }
