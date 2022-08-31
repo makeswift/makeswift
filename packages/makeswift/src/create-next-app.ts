af6311d58fef3160d4311e9121bd01ecae9e7a34
Alex Woods <alexhwoods@gmail.com>
2022-08-31 18:12:00 -0400

    fix: error if create-next-app fails

--- packages/makeswift/src/create-next-app.ts
+++ packages/makeswift/src/create-next-app.ts
@@ -4,8 +4,12 @@ import { coerceExampleToUrl } from './utils/coerce-example-to-URL'
 export function createNextApp({ dir, example }: { dir: string; example: string }): void {
   const url = coerceExampleToUrl(example)
 
-  spawn.sync('npx', ['--yes', 'create-next-app', '--example', url, dir], {
+  const output = spawn.sync('npx', ['--yes', 'create-next-app', '--example', url, dir], {
     stdio: 'inherit',
     cwd: __dirname,
   })
+
+  if (output.status === 1) {
+    throw Error('Unable to create a Next.js app. There should be output above.')
+  }
 }
