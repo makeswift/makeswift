48eb8d97379fc85aba2fb7308187129cac714da7
Fikri Karim <fkfikrikarim@gmail.com>
2022-09-02 15:01:06 +0700

    fix: text component not working on Chrome 105

---
+++ .changeset/pretty-dancers-suffer.md
@@ -0,0 +1,5 @@
+---
+"@makeswift/runtime": patch
+---
+
+Fix Text component not working on Chrome 105.
--- packages/runtime/src/components/builtin/Text/components/RichTextEditor/index.tsx
+++ packages/runtime/src/components/builtin/Text/components/RichTextEditor/index.tsx
@@ -28,6 +28,10 @@ export const RichTextEditor = styled(
     return (
       <SlateEditor
         {...restOfProps}
+        // Workaround because our Slate editor is broken on Chrome 105
+        // Problem: https://linear.app/makeswift/issue/PRD-434/our-rich-text-component-breaks-in-the-latest-version-of-chrome
+        // Workaround: https://github.com/ianstormtaylor/slate/issues/5110#issuecomment-1234951122
+        style={{ WebkitUserModify: undefined }}
         ref={ref}
         autoFocus={false}
         plugins={plugins}
