e737cc7a98f2d6aa4db920533731a96f63028250
Miguel Oller <miguel@makeswift.com>
2022-09-13 18:56:05 -0400

    feat: set compiler.styledComponents to true

---
+++ .changeset/poor-rivers-run.md
@@ -0,0 +1,5 @@
+---
+"@makeswift/next-plugin": patch
+---
+
+Set compiler.styledComponents to true in Makeswift Next.js plugin
--- packages/makeswift-next-plugin/index.js
+++ packages/makeswift-next-plugin/index.js
@@ -3,11 +3,21 @@ const withTmInitializer = require('next-transpile-modules')
 const NEXT_IMAGE_DOMAINS = ['s.mkswft.com']
 const NEXT_TRANSPILE_MODULES_MODULES = ['@makeswift/runtime']
 
-module.exports = ({ resolveSymlinks } = {}) => (nextConfig = {}) =>
-  withTmInitializer(NEXT_TRANSPILE_MODULES_MODULES, { resolveSymlinks })({
-    ...nextConfig,
-    images: {
-      ...nextConfig.images,
-      domains: [...(nextConfig.images?.domains ?? []), ...NEXT_IMAGE_DOMAINS],
-    },
-  })
+module.exports =
+  ({ resolveSymlinks } = {}) =>
+  (nextConfig = {}) => {
+    /** @type {import('next').NextConfig} */
+    const enhancedConfig = {
+      ...nextConfig,
+      compiler: {
+        ...nextConfig.compiler,
+        styledComponents: true,
+      },
+      images: {
+        ...nextConfig.images,
+        domains: [...(nextConfig.images?.domains ?? []), ...NEXT_IMAGE_DOMAINS],
+      },
+    }
+
+    return withTmInitializer(NEXT_TRANSPILE_MODULES_MODULES, { resolveSymlinks })(enhancedConfig)
+  }
