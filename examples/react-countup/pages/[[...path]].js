e813087420f452044c797111e9c800ac0e3c00ea
Miguel Oller <miguel@makeswift.com>
2022-09-27 14:49:34 -0400

    chore: update react-countup example

--- examples/react-countup/pages/[[...path]].js
+++ examples/react-countup/pages/[[...path]].js
@@ -1,7 +1,35 @@
 import '../lib/makeswift/register-components'
 
-export {
-  getStaticPaths,
-  getStaticProps,
-  Page as default,
-} from '@makeswift/runtime/next'
+import { Makeswift } from '@makeswift/runtime/next'
+
+import { Page as MakeswiftPage } from '@makeswift/runtime/next'
+
+export async function getStaticPaths() {
+  const makeswift = new Makeswift(process.env.MAKESWIFT_SITE_API_KEY)
+  const pages = await makeswift.getPages()
+
+  return {
+    paths: pages.map((page) => ({
+      params: {
+        path: page.path.split('/').filter((segment) => segment !== ''),
+      },
+    })),
+    fallback: 'blocking',
+  }
+}
+
+export async function getStaticProps(ctx) {
+  const makeswift = new Makeswift(process.env.MAKESWIFT_SITE_API_KEY)
+  const path = '/' + (ctx.params?.path ?? []).join('/')
+  const snapshot = await makeswift.getPageSnapshot(path, {
+    preview: ctx.preview,
+  })
+
+  if (snapshot == null) return { notFound: true }
+
+  return { props: { snapshot } }
+}
+
+export default function Page({ snapshot }) {
+  return <MakeswiftPage snapshot={snapshot} />
+}
