3f0f42d80386f527e9733955a4f9f7dfb6b68128
Alex Woods <alexhwoods@gmail.com>
2022-10-26 11:02:45 -0400

    feat: user can select example in UI during CLI setup process

---
+++ .changeset/famous-hats-eat.md
@@ -0,0 +1,5 @@
+---
+'makeswift': patch
+---
+
+Add ability for user to select example during setup process with `npx makeswift` CLI.
--- packages/makeswift/src/init.ts
+++ packages/makeswift/src/init.ts
@@ -16,7 +16,7 @@ const MAKESWIFT_APP_ORIGIN = process.env.MAKESWIFT_APP_ORIGIN || 'https://app.ma
 const MAKESWIFT_API_ORIGIN = process.env.MAKESWIFT_API_ORIGIN
 const siteSelectionPath = 'select-site'
 
-type InitArgs = { example?: string; useNpm: boolean; usePnpm: boolean; env: string[] }
+type InitArgs = { example: string | undefined; useNpm: boolean; usePnpm: boolean; env: string[] }
 
 export default async function wrappedInit(name: string | undefined, args: InitArgs) {
   try {
@@ -33,85 +33,103 @@ export default async function wrappedInit(name: string | undefined, args: InitAr
 
 async function init(
   name: string | undefined,
-  { example = 'basic-typescript', useNpm, usePnpm, env }: InitArgs,
+  { example: passedInExample, useNpm, usePnpm, env }: InitArgs,
 ): Promise<void> {
-  if (useNpm && usePnpm) {
-    throw new MakeswiftError(
-      'Cannot use both --use-npm and --use-pnpm args. Choose 1 package manager.',
-    )
+  function validate() {
+    if (useNpm && usePnpm) {
+      throw new MakeswiftError(
+        'Cannot use both --use-npm and --use-pnpm args. Choose 1 package manager.',
+      )
+    }
   }
 
+  async function performHandshake({
+    usingExistingNextApp,
+  }: {
+    usingExistingNextApp: boolean
+  }): Promise<{ nextAppPort: number; envLocal: string; example: string | null }> {
+    const handshakePort = await detect(5600)
+    const nextAppPort = await detect(3000)
+
+    const callbackUrl = `http://localhost:${handshakePort}/${siteSelectionPath}`
+    // Handshake Step 1
+    const selectSiteUrl = new URL(`${MAKESWIFT_APP_ORIGIN}/cli/select-site`)
+    selectSiteUrl.searchParams.set('project_name', projectName)
+    selectSiteUrl.searchParams.set('callback_url', callbackUrl)
+    selectSiteUrl.searchParams.set('using_existing_next_app', String(usingExistingNextApp))
+    passedInExample && selectSiteUrl.searchParams.set('example', passedInExample)
+
+    const selectSiteUrlString = selectSiteUrl.toString()
+    console.log(
+      `\nOpening your browser at ${chalk.blue(
+        selectSiteUrlString,
+      )}\n\nIf anything happens, re-open that URL.`,
+    )
+    await open(selectSiteUrlString)
+
+    // Handshake Step 2 - the browser goes to `callbackUrl`
+    const nextAppUrl = `http://localhost:${nextAppPort}`
+    const redirectUrl = new URL(`${MAKESWIFT_APP_ORIGIN}/cli/link-site`)
+    redirectUrl.searchParams.set('host_url', nextAppUrl)
+
+    // Handshake Step 3 - we redirect the browser to redirectUrl
+    const { siteApiKey, example } = await getSiteMetadata({
+      port: handshakePort,
+      redirectUrl: redirectUrl.toString(),
+    })
+
+    // In the background, we're setting up the Next app with the API key
+    // and starting the app at `nextAppPort`
+    const envLocal = buildLocalEnvFile(
+      {
+        MAKESWIFT_SITE_API_KEY: siteApiKey,
+        MAKESWIFT_API_ORIGIN,
+      },
+      env,
+    )
+
+    // Handshake Step 4 - Makeswift redirects to the builder with the site open,
+    //                    with the host using `nextAppUrl` for the builder
+
+    return { nextAppPort, envLocal, example }
+  }
+
+  validate()
+
   const { directory: nextAppDir, name: projectName } = await getProjectName(name)
+  const usingExistingNextApp = isNextApp(nextAppDir)
 
-  if (isNextApp(nextAppDir)) {
-    checkForConflictingFiles({ dir: nextAppDir })
+  checkForConflictingFiles({ dir: nextAppDir })
 
+  const { nextAppPort, envLocal, example } = await performHandshake({ usingExistingNextApp })
+
+  if (usingExistingNextApp) {
     await integrateNextApp({ dir: nextAppDir })
   } else {
     createNextApp({
       dir: nextAppDir,
-      example,
+      example: example || 'basic-typescript',
       useNpm,
       usePnpm,
     })
   }
 
-  const handshakePort = await detect(5600)
-  const nextAppPort = await detect(3000)
-
-  const callbackUrl = `http://localhost:${handshakePort}/${siteSelectionPath}`
-  // Handshake Step 1
-  const selectSiteUrl = new URL(`${MAKESWIFT_APP_ORIGIN}/cli/select-site`)
-  selectSiteUrl.searchParams.set('project_name', projectName)
-  selectSiteUrl.searchParams.set('callback_url', callbackUrl)
-
-  const selectSiteUrlString = selectSiteUrl.toString()
-  console.log(
-    `\nOpening your browser at ${chalk.blue(
-      selectSiteUrlString,
-    )}\n\nIf anything happens, re-open that URL.`,
-  )
-  await open(selectSiteUrlString)
-
-  // Handshake Step 2 - the browser goes to `callbackUrl`
-  const nextAppUrl = `http://localhost:${nextAppPort}`
-  const redirectUrl = new URL(`${MAKESWIFT_APP_ORIGIN}/cli/link-site`)
-  redirectUrl.searchParams.set('host_url', nextAppUrl)
-
-  // Handshake Step 3 - we redirect the browser to redirectUrl
-  const { siteApiKey } = await getSiteApiKey({
-    port: handshakePort,
-    redirectUrl: redirectUrl.toString(),
-  })
-
-  // In the background, we're setting up the Next app with the API key
-  // and starting the app at `nextAppPort`
-  const envLocal = buildLocalEnvFile(
-    {
-      MAKESWIFT_SITE_API_KEY: siteApiKey,
-      MAKESWIFT_API_ORIGIN,
-    },
-    env,
-  )
   fs.writeFileSync(`${nextAppDir}/.env.local`, envLocal)
 
   spawn.sync('yarn', ['dev', '--port', nextAppPort.toString()], {
     stdio: 'inherit',
     cwd: nextAppDir,
   })
-
-  // Handshake Step 4 - Makeswift redirects to the builder with the site open,
-  //                    with the host using `nextAppUrl` for the builder
 }
 
-async function getSiteApiKey({
+async function getSiteMetadata({
   port,
   redirectUrl,
 }: {
   port: number
   redirectUrl: string
-}): Promise<{ siteApiKey: string }> {
-  return new Promise<{ siteApiKey: string }>((resolve, reject) => {
+}): Promise<{ siteApiKey: string; example: string | null }> {
+  return new Promise<{ siteApiKey: string; example: string | null }>((resolve, reject) => {
     const sockets: Socket[] = []
     const server = http
       .createServer((req, res) => {
@@ -130,6 +148,8 @@ async function getSiteApiKey({
           return
         }
 
+        const example = queryParams.get('example')
+
         // add the api key in the redirect URL
         const destinationURL = new URL(redirectUrl)
         destinationURL.searchParams.set('api_key', siteApiKey)
@@ -143,6 +163,7 @@ async function getSiteApiKey({
             }
             resolve({
               siteApiKey,
+              example,
             })
           })
 
@@ -156,7 +177,7 @@ async function getSiteApiKey({
       })
       .listen(port)
 
-    server.on('connection', function(socket: Socket) {
+    server.on('connection', function (socket: Socket) {
       sockets.push(socket)
     })
   })
