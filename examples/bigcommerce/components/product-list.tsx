11864ecda33e967ecf5c9eafc2b55c465a32bcfd
Fikri Karim <fkfikrikarim@gmail.com>
2022-11-02 16:11:23 +0700

    fix: update <Image/> components size to be 1:1
    It looks like all the images that come from the integration
    example have aspect ratio of 1:1.

--- examples/bigcommerce/components/product-list.tsx
+++ examples/bigcommerce/components/product-list.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
 
 import { useProducts } from 'lib/products-context'
@@ -24,13 +24,12 @@ export function ProductList({ className, categoryEntityId, count }: Props) {
             <Link href={`/product/${product.entityId}`} key={product.entityId}>
               <a className="w-[325px] max-w-full group">
                 <div className="relative z-0">
-                  <Image
+                  <img
                     className=""
                     src={product.defaultImage.urlOriginal}
                     alt={product.defaultImage.altText}
                     width="325"
-                    height="400"
-                  ></Image>
+                  />
                   <div className="absolute inset-0 group-hover:bg-white/10 group-hover:backdrop-saturate-10 transition-all"></div>
                 </div>
                 <div className="pt-3 flex flex-col">
