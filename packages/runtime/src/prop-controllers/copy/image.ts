15ef55537264aef504b24e18b4db7347acdd2300
Alex Woods <alexhwoods@gmail.com>
2022-11-30 15:54:46 -0500

    feat: add image copy function

---
+++ packages/runtime/src/prop-controllers/copy/image.ts
@@ -0,0 +1,42 @@
+import { CopyContext, ReplacementContext } from '../../state/react-page'
+import { ImageValue } from '../descriptors'
+
+export function copy(value: ImageValue, context: CopyContext): ImageValue {
+  if (value == null) return value
+
+  return context.replacementContext.fileIds.get(value) ?? value
+}
+
+if (import.meta.vitest) {
+  const { describe, test, expect } = import.meta.vitest
+
+  describe.concurrent('image copy', () => {
+    test('image is replaced by a one in replacement context', () => {
+      // Arrange
+      const data: ImageValue = 'file-id'
+      const expected = JSON.parse(JSON.stringify(data).replace('file-id', 'testing'))
+
+      const replacementContext = {
+        elementHtmlIds: new Set(),
+        elementKeys: new Map(),
+        swatchIds: new Map(),
+        fileIds: new Map([['file-id', 'testing']]),
+        typographyIds: new Map(),
+        tableIds: new Map(),
+        tableColumnIds: new Map(),
+        pageIds: new Map(),
+        globalElementIds: new Map(),
+        globalElementData: new Map(),
+      }
+
+      // Act
+      const result = copy(data, {
+        replacementContext: replacementContext as ReplacementContext,
+        copyElement: node => node,
+      })
+
+      // Assert
+      expect(result).toMatchObject(expected)
+    })
+  })
+}
