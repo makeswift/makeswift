1a139b206a8211af2a13468c779a5b9489166ff7
Miguel Oller <miguel@makeswift.com>
2022-12-05 12:55:18 -0500

    feat: useStyle for Divider component
    Resolves PRD-796.

--- packages/runtime/src/components/builtin/Divider/Divider.tsx
+++ packages/runtime/src/components/builtin/Divider/Divider.tsx
@@ -1,18 +1,15 @@
 import { forwardRef, Ref } from 'react'
-import styled, { css } from 'styled-components'
 
-import { cssMediaRules, cssMargin, cssWidth } from '../../utils/cssMediaRules'
 import {
-  ResponsiveValue,
   ElementIDValue,
-  MarginValue,
   ResponsiveLengthValue,
   ResponsiveSelectValue,
-  WidthValue,
 } from '../../../prop-controllers/descriptors'
 import { colorToString } from '../../utils/colorToString'
-import { ColorValue as Color } from '../../utils/types'
 import { ResponsiveColor } from '../../../runtimes/react/controls'
+import { useStyle } from '../../../runtimes/react/use-style'
+import { cx } from '@emotion/css'
+import { responsiveStyle } from '../../utils/responsive-style'
 
 type DividerVariant = 'solid' | 'dashed' | 'dotted' | 'blended'
 
@@ -21,89 +18,73 @@ type Props = {
   variant?: ResponsiveSelectValue<DividerVariant>
   thickness?: ResponsiveLengthValue
   color?: ResponsiveColor | null
-  width?: WidthValue
-  margin?: MarginValue
+  width?: string
+  margin?: string
 }
-
-// IE11 has a bug with Flexbox vertical centering with min height if height is not set.
-// Wrapping it in another flex container fixes it for some reason, read more here:
-// https://stackoverflow.com/questions/19371626/flexbox-not-centering-vertically-in-ie
-const IE11MinHeightContainer = styled.div.withConfig({
-  shouldForwardProp: prop => !['width', 'margin'].includes(prop),
-})<{ width: Props['width']; margin: Props['margin'] }>`
-  display: flex;
-  width: 100%;
-  ${cssWidth()}
-  ${cssMargin()}
-`
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  min-height: 40px;
-  width: 100%;
-`
-
-const Line = styled.div.withConfig({
-  shouldForwardProp: prop => !['variant', 'color', 'thickness'].includes(prop),
-})<{
-  variant: Props['variant']
-  color?: ResponsiveValue<Color> | null
-  thickness: Props['thickness']
-}>`
-  ${p =>
-    cssMediaRules(
-      [p.variant, p.thickness, p.color] as const,
-      ([
-        variant = 'solid',
-        thickness = { value: 1, unit: 'px' },
-        color = { swatch: { hue: 0, saturation: 0, lightness: 0 }, alpha: 1 },
-      ]) => {
-        switch (variant) {
-          case 'solid':
-          case 'dashed':
-          case 'dotted':
-            return css`
-              border-bottom-width: ${thickness == null
-                ? '1px'
-                : `${thickness.value}${thickness.unit}`};
-              border-bottom-style: ${variant};
-              border-bottom-color: ${colorToString(color)};
-            `
-
-          case 'blended':
-            return css`
-              height: ${`${thickness.value}${thickness.unit}`};
-              background: linear-gradient(
-                ${[
-                  '90deg',
-                  colorToString({ swatch: color.swatch, alpha: 0 }),
-                  colorToString(color),
-                  colorToString({ swatch: color.swatch, alpha: 0 }),
-                ].join(', ')}
-              );
-            `
-
-          default:
-            variant as never
-            throw new Error(`Invalid variant ${variant}.`)
-        }
-      },
-    )}
-`
 
 const Divider = forwardRef(function Divider(
   { id, variant, thickness, color, width, margin }: Props,
   ref: Ref<HTMLDivElement>,
 ) {
   return (
-    <IE11MinHeightContainer ref={ref} id={id} width={width} margin={margin}>
-      <Container>
-        {/* @ts-expect-error: HTMLDivElement `color` attribute conflicts with prop */}
-        <Line variant={variant} thickness={thickness} color={color} />
-      </Container>
-    </IE11MinHeightContainer>
+    // IE11 has a bug with Flexbox vertical centering with min height if height is not set.
+    // Wrapping it in another flex container fixes it for some reason, read more here:
+    // https://stackoverflow.com/questions/19371626/flexbox-not-centering-vertically-in-ie
+    <div
+      ref={ref}
+      id={id}
+      className={cx(useStyle({ display: 'flex', width: '100%' }), width, margin)}
+    >
+      <div
+        className={useStyle({
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          minHeight: 40,
+          width: '100%',
+        })}
+      >
+        <div
+          className={useStyle(
+            responsiveStyle(
+              [variant, thickness, color] as const,
+              ([
+                variant = 'solid',
+                thickness = { value: 1, unit: 'px' },
+                color = { swatch: { hue: 0, saturation: 0, lightness: 0 }, alpha: 1 },
+              ]) => {
+                switch (variant) {
+                  case 'solid':
+                  case 'dashed':
+                  case 'dotted':
+                    return {
+                      borderBottomWidth:
+                        thickness == null ? 1 : `${thickness.value}${thickness.unit}`,
+                      borderBottomStyle: variant,
+                      borderBottomColor: colorToString(color),
+                    }
+
+                  case 'blended':
+                    return {
+                      height: `${thickness.value}${thickness.unit}`,
+                      background: `linear-gradient(${[
+                        '90deg',
+                        colorToString({ swatch: color.swatch, alpha: 0 }),
+                        colorToString(color),
+                        colorToString({ swatch: color.swatch, alpha: 0 }),
+                      ].join(', ')})`,
+                    }
+
+                  default:
+                    variant as never
+                    throw new Error(`Invalid variant ${variant}.`)
+                }
+              },
+            ),
+          )}
+        />
+      </div>
+    </div>
   )
 })
 
