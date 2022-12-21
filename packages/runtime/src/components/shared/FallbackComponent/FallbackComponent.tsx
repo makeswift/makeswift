27bc6400346005b0a83825e3985d97a363cb13a6
Miguel Oller <miguel@makeswift.com>
2022-12-21 18:46:40 -0500

    feat: useStyle for FallbackComponent

--- packages/runtime/src/components/shared/FallbackComponent/FallbackComponent.tsx
+++ packages/runtime/src/components/shared/FallbackComponent/FallbackComponent.tsx
@@ -1,21 +1,6 @@
 import { forwardRef, Ref } from 'react'
-import styled from 'styled-components'
+import { useStyle } from '../../../runtimes/react/use-style'
 import { ReactComponent as Warning20 } from '../../icons/warning-20.svg'
-
-const ErrorDiv = styled('div')`
-  width: 100%;
-  height: 54px;
-  background-color: #fcedf2;
-  border-radius: 6px;
-  padding: 16px;
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  color: #c73e6d;
-  fill: currentColor;
-  font-family: Heebo, sans-serif;
-  font-size: 16px;
-`
 
 type Props = {
   text: string
@@ -26,9 +11,25 @@ export const FallbackComponent = forwardRef(function FallbackComponent(
   ref: Ref<HTMLDivElement>,
 ) {
   return (
-    <ErrorDiv ref={ref}>
+    <div
+      ref={ref}
+      className={useStyle({
+        width: '100%',
+        height: 54,
+        backgroundColor: '#fcedf2',
+        borderRadius: 6,
+        padding: 16,
+        display: 'flex',
+        alignItems: 'center',
+        gap: 8,
+        color: '#c73e6d',
+        fill: 'currentColor',
+        fontFamily: 'Heebo, sans-serif',
+        fontSize: 16,
+      })}
+    >
       <Warning20 />
       <span>{text}</span>
-    </ErrorDiv>
+    </div>
   )
 })
