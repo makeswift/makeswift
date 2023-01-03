b389d8d0e2f860127f1579ccc2a30ebf7e7cbda3
Joshua Wootonn <joshuawootonn@gmail.com>
2023-01-03 17:51:49 -0600

    feat: add 404 page to bigcommerce example

--- examples/bigcommerce/pages/_error.tsx
+++ examples/bigcommerce/pages/_error.tsx
@@ -1,9 +1,14 @@
 import Error, { ErrorProps } from 'next/error'
-import { useLayoutEffect } from 'react'
+import { useEffect } from 'react'
 import OfflinePage from './_offline'
 
+/*
+ * This error page catches clientside errors that occur from next-pwa throwing on offline pages that use getStaticProps. Related issue: https://github.com/shadowwalker/next-pwa/issues/440
+ * Using an error boundary didn't work.
+ * Since I am grouping all errors by adding a _error page, I also added a 404 page to more specifically handle that case.
+ */
 export default function ErrorPage({ statusCode }: ErrorProps) {
-  useLayoutEffect(() => {
+  useEffect(() => {
     if (statusCode == null && window.navigator.onLine === false) {
       window.location.reload()
     }
