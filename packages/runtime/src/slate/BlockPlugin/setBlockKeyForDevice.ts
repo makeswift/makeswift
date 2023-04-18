6719613a95d8eebbb1224a7199e9a08ccf7a8b70
Fikri Karim <fkfikrikarim@gmail.com>
2023-04-18 17:04:50 +0700

    feat: pass breakpoints to setBlockKeyForDevice

--- packages/runtime/src/slate/BlockPlugin/setBlockKeyForDevice.ts
+++ packages/runtime/src/slate/BlockPlugin/setBlockKeyForDevice.ts
@@ -1,11 +1,12 @@
 import { Editor, Transforms } from 'slate'
-import { findDeviceOverride } from '../../components/utils/devices'
 import { Device } from '../../controls/types'
+import { Breakpoints, findBreakpointOverride } from '../../state/modules/breakpoints'
 import { ElementUtils } from '../utils/element'
 import { EditableBlockKey, EditableBlockValue } from './types'
 
 export function setBlockKeyForDevice(
   editor: Editor,
+  breakpoints: Breakpoints,
   deviceId: Device,
   key: EditableBlockKey,
   value: EditableBlockValue,
@@ -20,7 +21,7 @@ export function setBlockKeyForDevice(
   for (const [node, path] of rootElements) {
     if (ElementUtils.isBlock(node)) {
       const deviceValues = node[key] ?? []
-      const currentDeviceValue = findDeviceOverride(deviceValues, deviceId, v => v)
+      const currentDeviceValue = findBreakpointOverride(breakpoints, deviceValues, deviceId, v => v)
       const nextDeviceValue = {
         ...currentDeviceValue,
         deviceId,
