3f95a8cfb4f71e4bdcabc4e025727b5001995978
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-12-18 13:53:01 -0500

    feat: add type to color control

---
+++ .changeset/chilled-llamas-dream.md
@@ -0,0 +1,5 @@
+---
+'@makeswift/runtime': patch
+---
+
+Add type and version to the Color control
--- packages/runtime/src/controls/color.ts
+++ packages/runtime/src/controls/color.ts
@@ -1,7 +1,22 @@
-import { CopyContext } from '../state/react-page'
-import { ColorData } from './types'
+import { match } from 'ts-pattern'
+import type { CopyContext } from '../state/react-page'
+import type { ColorData } from './types'
 
-export type ColorControlData = ColorData
+// TODO: We don't know where this is going to live yet, so exporting it
+// globally for now and reexporting it as a namespaced export below.
+export const ControlDataTypeKey = '@@makeswift/type'
+
+export const ColorControlDataTypeKey = ControlDataTypeKey
+
+export const ColorControlDataTypeValueV1 = 'color::v1'
+
+export type ColorControlDataV0 = ColorData
+
+export type ColorControlDataV1 = ColorControlDataV0 & {
+  [ControlDataTypeKey]: typeof ColorControlDataTypeValueV1
+}
+
+export type ColorControlData = ColorControlDataV0 | ColorControlDataV1
 
 export const ColorControlType = 'makeswift::controls::color'
 
@@ -15,20 +30,26 @@ type ColorControlConfig = {
 export type ColorControlDefinition<C extends ColorControlConfig = ColorControlConfig> = {
   type: typeof ColorControlType
   config: C
+  version?: 1
 }
 
 export function Color<C extends ColorControlConfig>(config = {} as C): ColorControlDefinition<C> {
-  return { type: ColorControlType, config }
+  return { type: ColorControlType, config, version: 1 }
 }
 
 export function copyColorData(
-  value: ColorData | undefined,
+  value: ColorControlData | undefined,
   context: CopyContext,
-): ColorData | undefined {
+): ColorControlData | undefined {
   if (value == null) return value
 
-  return {
-    ...value,
-    swatchId: context.replacementContext.swatchIds.get(value.swatchId) ?? value.swatchId,
-  }
+  return match(value)
+    .with({ [ControlDataTypeKey]: ColorControlDataTypeValueV1 }, val => ({
+      ...val,
+      swatchId: context.replacementContext.swatchIds.get(val.swatchId) ?? val.swatchId,
+    }))
+    .otherwise(val => ({
+      ...val,
+      swatchId: context.replacementContext.swatchIds.get(val.swatchId) ?? val.swatchId,
+    }))
 }
