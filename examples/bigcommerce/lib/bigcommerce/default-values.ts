a5f7b945afacf87cbcb14bcdb7a4341a79b58381
joshua wootonn <joshuawootonn@gmail.com>
2024-01-05 12:23:07 -0500

    chore: update BC example default product

--- examples/bigcommerce/lib/bigcommerce/default-values.ts
+++ examples/bigcommerce/lib/bigcommerce/default-values.ts
@@ -1,46 +1,47 @@
 import { CartResponse, ProductFragment } from './types'
 
 export const DEFAULT_PRODUCT: ProductFragment = {
-  entityId: 113,
-  name: 'Monstera',
+  entityId: 112,
+  name: 'Monsterra',
   description:
     '<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a.  Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis.</p>',
   defaultImage: {
     urlOriginal:
-      'https://cdn11.bigcommerce.com/s-uvhswop3wh/images/stencil/original/products/113/396/image_2__07829.1660659630.png',
-    altText: 'monstera plant',
+      'https://cdn11.bigcommerce.com/s-5yjdhtv55p/images/stencil/original/products/112/376/plant-1__92670.1704473318.jpg',
+    altText: 'Monsterra Plant',
   },
   images: {
     edges: [
       {
         node: {
           urlOriginal:
-            'https://cdn11.bigcommerce.com/s-uvhswop3wh/images/stencil/original/products/113/396/image_2__07829.1660659630.png',
-          altText: 'monstera plant',
+            'https://cdn11.bigcommerce.com/s-5yjdhtv55p/images/stencil/original/products/112/376/plant-1__92670.1704473318.jpg',
+          altText: 'Monsterra Plant',
         },
       },
     ],
   },
   prices: {
     price: {
-      value: 79,
+      value: 50.0,
       currencyCode: 'USD',
     },
   },
+
   categories: {
     edges: [
       {
         node: {
-          entityId: 23,
-          name: 'Shop All',
-          path: '/shop-all/',
+          entityId: 25,
+          name: 'Plants',
+          path: '/plants/',
         },
       },
       {
         node: {
           entityId: 24,
-          name: 'Plants',
-          path: '/plants/',
+          name: 'All Products',
+          path: '/all-products/',
         },
       },
     ],
