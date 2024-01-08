9c1941ff278b5be2c8e3ef44ec352f4ffd6b6057
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-01-08 13:41:47 -0500

    feat: add type field to TextArea control

--- packages/runtime/src/controls/text-area.ts
+++ packages/runtime/src/controls/text-area.ts
@@ -1,4 +1,17 @@
-export type TextAreaControlData = string
+import { ControlDataTypeKey } from "./control-data-type-key"
+
+export const TextAreaControlDataTypeKey = ControlDataTypeKey
+
+export const TextAreaControlDataTypeValueV1 = 'text-area::v1'
+
+export type TextAreaControlDataV0 = string
+
+export type TextAreaControlDataV1 = {
+  [TextAreaControlDataTypeKey]: typeof TextAreaControlDataTypeValueV1
+  value: string,
+}
+
+export type TextAreaControlData = TextAreaControlDataV0 | TextAreaControlDataV1
 
 export const TextAreaControlType = 'makeswift::controls::text-area'
 
@@ -11,10 +24,11 @@ type TextAreaControlConfig = {
 export type TextAreaControlDefinition<C extends TextAreaControlConfig = TextAreaControlConfig> = {
   type: typeof TextAreaControlType
   config: C
+  version?: 1
 }
 
 export function TextArea<C extends TextAreaControlConfig>(
   config: C = {} as C,
 ): TextAreaControlDefinition<C> {
-  return { type: TextAreaControlType, config }
+  return { type: TextAreaControlType, config, version: 1 }
 }
