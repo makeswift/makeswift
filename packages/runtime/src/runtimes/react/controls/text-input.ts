dd7c1d1165989b238b579262f2669b6e46cc3a98
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-01-08 13:01:11 -0500

    feat: add type field to TextInput control

--- packages/runtime/src/runtimes/react/controls/text-input.ts
+++ packages/runtime/src/runtimes/react/controls/text-input.ts
@@ -1,4 +1,5 @@
-import { TextInputControlData, TextInputControlDefinition } from '../../../controls'
+import { match } from 'ts-pattern'
+import { TextInputControlData, TextInputControlDataTypeKey, TextInputControlDataTypeValueV1, TextInputControlDefinition } from '../../../controls'
 
 export type TextInputControlValue<T extends TextInputControlDefinition> =
   undefined extends T['config']['defaultValue'] ? string | undefined : string
@@ -7,5 +8,9 @@ export function useTextInputValue<T extends TextInputControlDefinition>(
   data: TextInputControlData | undefined,
   definition: T,
 ): TextInputControlValue<T> {
-  return (data ?? definition.config.defaultValue) as TextInputControlValue<T>
+  const value: string | undefined = match(data)
+    .with({ [TextInputControlDataTypeKey]: TextInputControlDataTypeValueV1 }, (val) => val.value)
+    .otherwise(val => val) ?? definition.config.defaultValue
+
+  return value as TextInputControlValue<T>
 }
