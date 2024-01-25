4201fd64411f770d87e5b3f7654cfcb3f04cca41
joshua wootonn <joshuawootonn@gmail.com>
2024-01-25 16:28:08 -0500

    feat: add jest configuration and first test

---
+++ test/pages/hello-world-page.tsx
@@ -0,0 +1,7 @@
+export default function HelloWorldPage() {
+  return (
+    <main className="flex font-sans min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-5xl font-bold italic">Makeswift</h1>
+    </main>
+  )
+}
