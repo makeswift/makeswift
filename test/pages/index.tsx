89eaf63dfa79463956602dc05f244d79dbed9219
joshua wootonn <joshuawootonn@gmail.com>
2024-01-26 08:43:42 -0500

    feat: update index page with links to all pages

--- test/pages/index.tsx
+++ test/pages/index.tsx
@@ -1,3 +1,31 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <main>Home</main>
+  return (
+    <main className="flex justify-center items-center flex-col h-screen w-screen">
+      <div className="prose w-96">
+        <h1>Test URLs</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Test</th>
+              <th>Pages URL</th>
+              <th>App URL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>hello-world</td>
+              <td>
+                <Link href="/hello-world-page">pages</Link>
+              </td>
+              <td>
+                <Link href="/hello-world-app">app</Link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+  )
 }
--- test/tailwind.config.ts
+++ test/tailwind.config.ts
@@ -13,6 +13,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 }
 export default config
