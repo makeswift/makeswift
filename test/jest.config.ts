fba0a96a4b3220b27f7604353b3a5e6fbad56048
joshua wootonn <joshuawootonn@gmail.com>
2024-01-26 08:36:40 -0500

    feat: add playwright configuration and first test

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -9,6 +9,7 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/__specs__/'],
 }
 
 export default createJestConfig(config)
