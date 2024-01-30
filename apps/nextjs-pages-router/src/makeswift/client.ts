63b83fc37df3e82d5d1f946285e820e1e4249e13
Miguel Oller <maoa22@gmail.com>
2024-01-30 17:08:14 -0500

    feat: set up pages router app

---
+++ apps/nextjs-pages-router/src/makeswift/client.ts
@@ -0,0 +1,6 @@
+import { Makeswift } from '@makeswift/runtime/next'
+
+import { runtime } from '@/makeswift/runtime'
+import { MAKESWIFT_SITE_API_KEY } from '@/makeswift/env'
+
+export const client = new Makeswift(MAKESWIFT_SITE_API_KEY, { runtime })
