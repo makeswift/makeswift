f3323a04c23971157b826e28d854cfec8ceccb8d
Miguel Oller <maoa22@gmail.com>
2024-01-31 15:07:56 -0500

    fix: pass runtime to API handler in pages app

--- apps/nextjs-pages-router/src/pages/api/makeswift/[...makeswift].ts
+++ apps/nextjs-pages-router/src/pages/api/makeswift/[...makeswift].ts
@@ -1,4 +1,5 @@
 import { MAKESWIFT_SITE_API_KEY } from '@/makeswift/env'
+import { runtime } from '@/makeswift/runtime'
 import { MakeswiftApiHandler } from '@makeswift/runtime/next/server'
 
-export default MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY)
+export default MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY, { runtime })
