7addffdbd86c7481c4de7a308871e0a9c389aea3
joshua wootonn <joshuawootonn@gmail.com>
2024-02-08 16:44:58 -0500

    feat: spike localization for app router kitchen sink

--- apps/nextjs-pages-router/next.config.mjs
+++ apps/nextjs-pages-router/next.config.mjs
@@ -5,6 +5,10 @@ const withMakeswift = createWithMakeswift()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en-US", "en-FR", "fr-FR"],
+    defaultLocale: "en-US",
+  },
 }
 
 export default withMakeswift(nextConfig)
