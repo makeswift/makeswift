bd1f35f1f106e5b2bf8d942d2423dc63694d7546
joshua wootonn <joshuawootonn@gmail.com>
2024-02-13 17:25:44 -0500

    feat: spike localization for app router kitchen sink

--- apps/nextjs-app-router/src/app/[lang]/[[...path]]/page.tsx
+++ apps/nextjs-app-router/src/app/[lang]/[[...path]]/page.tsx
@@ -3,21 +3,26 @@ import '@/makeswift/components'
 import { getSiteVersion } from '@makeswift/runtime/next/server'
 import { notFound } from 'next/navigation'
 import { Page as MakeswiftPage } from '@makeswift/runtime/next'
+import { locales } from '@/localization'
 
-type ParsedUrlQuery = { path?: string[] }
+type ParsedUrlQuery = { lang: string; path?: string[] }
 
 export async function generateStaticParams() {
   const pages = await client.getPages()
 
-  return pages.map((page) => ({
-    path: page.path.split('/').filter((segment) => segment !== ''),
-  }))
+  return pages.flatMap((page) =>
+    locales.map((locale) => ({
+      path: page.path.split('/').filter((segment) => segment !== ''),
+      lang: locale,
+    })),
+  )
 }
 
 export default async function Page({ params }: { params: ParsedUrlQuery }) {
   const path = '/' + (params?.path ?? []).join('/')
   const snapshot = await client.getPageSnapshot(path, {
     siteVersion: getSiteVersion(),
+    locale: params.lang
   })
 
   if (snapshot == null) return notFound()
