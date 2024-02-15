81e747063d900323ec37687fe2afa4665a94fc0a
joshua wootonn <joshuawootonn@gmail.com>
2024-02-15 12:34:31 -0500

    feat: add `apiOrigin` and `appOrigin` for local development
    Without passing these values to the Makeswift api handler and client
    you get a cors error when running a local host within app.makeswift.com

--- apps/nextjs-app-router/src/app/api/makeswift/[...makeswift]/route.ts
+++ apps/nextjs-app-router/src/app/api/makeswift/[...makeswift]/route.ts
@@ -1,6 +1,9 @@
 import { MAKESWIFT_SITE_API_KEY } from '@/makeswift/env'
 import { MakeswiftApiHandler } from '@makeswift/runtime/next/server'
 
-const handler = MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY)
+const handler = MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY, {
+  apiOrigin: process.env.MAKESWIFT_API_ORIGIN,
+  appOrigin: process.env.MAKESWIFT_APP_ORIGIN,
+})
 
 export { handler as GET, handler as POST }
--- apps/nextjs-app-router/src/makeswift/client.ts
+++ apps/nextjs-app-router/src/makeswift/client.ts
@@ -3,4 +3,7 @@ import { Makeswift } from '@makeswift/runtime/next'
 import { runtime } from '@/makeswift/runtime'
 import { MAKESWIFT_SITE_API_KEY } from '@/makeswift/env'
 
-export const client = new Makeswift(MAKESWIFT_SITE_API_KEY, { runtime })
+export const client = new Makeswift(MAKESWIFT_SITE_API_KEY, {
+  runtime,
+  apiOrigin: process.env.MAKESWIFT_API_ORIGIN,
+})
