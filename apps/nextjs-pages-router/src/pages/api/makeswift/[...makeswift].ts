9c8a6f51195472b0eb429c1f927baf4a03927008
Arvin Poddar <arvin@makeswift.com>
2024-02-16 14:29:14 -0500

    feat: add app and api origins to pages router app

--- apps/nextjs-pages-router/src/pages/api/makeswift/[...makeswift].ts
+++ apps/nextjs-pages-router/src/pages/api/makeswift/[...makeswift].ts
@@ -2,4 +2,8 @@ import { MAKESWIFT_SITE_API_KEY } from '@/makeswift/env'
 import { runtime } from '@/makeswift/runtime'
 import { MakeswiftApiHandler } from '@makeswift/runtime/next/server'
 
-export default MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY, { runtime })
+export default MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY, {
+  runtime,
+  apiOrigin: process.env.MAKESWIFT_API_ORIGIN,
+  appOrigin: process.env.MAKESWIFT_APP_ORIGIN,
+})
