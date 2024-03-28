90af43c99dfd2c90334976f37f4d12a17d338872
joshua wootonn <joshuawootonn@gmail.com>
2024-03-28 14:47:34 -0400

    fix: fix tailwind config in basic typescript to include app router

--- examples/basic-typescript/tailwind.config.js
+++ examples/basic-typescript/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
