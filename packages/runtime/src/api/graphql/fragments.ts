6e80d5ddc66b88c72793cb6e305f2f9c5f0a071e
Aleksey Gurtovoy <agurtovoy@acm.org>
2024-09-24 00:57:45 -0500

    feat: add optional locale scope to the API resources state

--- packages/runtime/src/api/graphql/fragments.ts
+++
@@ -1,5 +0,0 @@
-import { DocumentNode } from 'graphql'
-import { APIResourceType } from '..'
-import * as FragmentDocumentNodes from './generated/fragment-document-nodes'
-
-export const Fragments: Record<APIResourceType, DocumentNode> = FragmentDocumentNodes
