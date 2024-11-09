58ab4c74f624bbbec550832166b669dc4d744638
Aleksey Gurtovoy <agurtovoy@acm.org>
2024-11-08 22:20:20 -0600

    fix(controls): fix stalled calls/memory leaks on repeated function deserialization

--- packages/controls/src/serialization/function-serialization.ts
+++ packages/controls/src/serialization/function-serialization.ts
@@ -6,7 +6,16 @@ export function isFunction(value: any): value is AnyFunction {
   return typeof value === 'function'
 }
 
+type ResolveCallPromise<T extends AnyFunction> = (
+  value: SerializedFunctionReturnType<T>,
+) => void
+
+type OnMessageHandler<T extends AnyFunction> = MessagePort['onmessage'] & {
+  newCall?(resolve: ResolveCallPromise<T>): number
+}
+
 export type SerializedFunction<T extends AnyFunction> = MessagePort & {
+  onmessage: OnMessageHandler<T>
   readonly [SerializedFunctionTag]: T
 }
 
@@ -44,30 +53,46 @@ export function serializeFunction<T extends AnyFunction>(
   return messageChannel.port2 as SerializedFunction<T>
 }
 
-export function deserializeFunction<T extends AnyFunction>(
-  serializedFunction: SerializedFunction<T>,
-): DeserializedFunction<T> {
-  type Result = SerializedFunctionReturnType<T>
-  type ResultMessageEvent = MessageEvent<[CallID, Result]>
-  type ResolveCallPromise = (value: Result | PromiseLike<Result>) => void
-
+function onmessageHandler<T extends AnyFunction>(): OnMessageHandler<T> {
+  type ResultMessageEvent = MessageEvent<
+    [CallID, SerializedFunctionReturnType<T>]
+  >
   let nextCallId = 0
-  const calls = new Map<CallID, ResolveCallPromise>()
+  const calls = new Map<CallID, ResolveCallPromise<T>>()
 
-  serializedFunction.onmessage = ({
+  const result: OnMessageHandler<T> = ({
     data: [callId, result],
   }: ResultMessageEvent) => {
     calls.get(callId)?.(result)
-
     calls.delete(callId)
+  }
+
+  result.newCall = (resolve: ResolveCallPromise<T>) => {
+    const callId = nextCallId++
+    calls.set(callId, resolve)
+    return callId
+  }
+
+  return result
+}
+
+export function deserializeFunction<T extends AnyFunction>(
+  serializedFunction: SerializedFunction<T>,
+): DeserializedFunction<T> {
+  if (serializedFunction.onmessage == null) {
+    serializedFunction.onmessage = onmessageHandler<T>()
   }
 
   return function deserializedFunction(...args) {
     return new Promise((resolve) => {
-      const callId = nextCallId++
+      const { newCall } = serializedFunction.onmessage
+      if (newCall == null) {
+        throw new Error(
+          `Deserialized function call failed: 'onmessage' handler is missing 'newCall' method`,
+        )
+      }
 
-      calls.set(callId, resolve)
-
+      const callId = newCall(resolve)
       serializedFunction.postMessage([callId, args])
     })
   }
