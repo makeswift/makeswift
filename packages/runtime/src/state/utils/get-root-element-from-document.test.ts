3a8a107f2d13ba6316b159cb9b014f8d352bf7aa
Arvin Poddar <arvin@makeswift.com>
2024-12-04 10:43:24 -0500

    refactor(runtime): pass initial root element directly
    Rather than passing metadata about whether the root element exists or not, we
    now allow the root element to be null, and also pass an initial root element
    field. This allows us to derive whether the root exists, as well as use this
    initial root to reset the data later.

---
+++ packages/runtime/src/state/utils/get-root-element-from-document.test.ts
@@ -0,0 +1,64 @@
+import { getRootElementFromDocument } from './get-root-element-from-document'
+import { Document } from '../react-page'
+
+describe('getRootElementFromDocument', () => {
+  test('returns rootElement for base documents', () => {
+    const document: Document = {
+      key: 'document-key',
+      locale: null,
+      rootElement: {
+        key: 'root-element',
+        type: 'DIV',
+        props: {},
+      },
+    }
+
+    const root = getRootElementFromDocument(document)
+    expect(root).toBe(document.rootElement)
+  })
+
+  test('returns root element if defined for embedded document', () => {
+    const document: Document = {
+      id: 'document-id',
+      key: 'document-key',
+      type: 'DOCUMENT',
+      name: 'Document',
+      locale: null,
+      initialRootElement: {
+        key: 'initial-root-element',
+        type: 'DIV',
+        props: {},
+      },
+      rootElement: {
+        key: 'root-element',
+        type: 'DIV',
+        props: {},
+      },
+      __type: 'EMBEDDED_DOCUMENT',
+    }
+
+    const root = getRootElementFromDocument(document)
+    expect(root).toBe(document.rootElement)
+  })
+
+  test('returns initial root element if root element is null', () => {
+    const document: Document = {
+      id: 'document-id',
+      key: 'document-key',
+      type: 'DOCUMENT',
+      name: 'Document',
+      locale: null,
+      initialRootElement: {
+        key: 'initial-root-element',
+        type: 'DIV',
+        props: {},
+      },
+      rootElement: null,
+      __type: 'EMBEDDED_DOCUMENT',
+      __version: 1,
+    }
+
+    const root = getRootElementFromDocument(document)
+    expect(root).toBe(document.initialRootElement)
+  })
+})
