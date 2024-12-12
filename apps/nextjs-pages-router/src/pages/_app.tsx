8a7a943f33f73bfa5291ee8f64fbac59f28b67a5
Joshua Wootonn <joshuawootonn@gmail.com>
2024-12-12 17:05:50 -0500

    feat: update `/pages` with font control demo

--- apps/nextjs-pages-router/src/pages/_app.tsx
+++ apps/nextjs-pages-router/src/pages/_app.tsx
@@ -1,22 +1,37 @@
 import { runtime } from '@/makeswift/runtime'
 import { ReactRuntimeProvider } from '@makeswift/runtime/next'
 import type { AppProps } from 'next/app'
+import { Grenze_Gotisch, Grenze } from 'next/font/google'
 
 import '@/makeswift/components'
 import '@/pages/global.css'
+
+const GrenzeGotischFont = Grenze_Gotisch({
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
+  variable: '--font-grenze-gotisch',
+})
+
+const GrenzeFont = Grenze({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+  variable: '--font-grenze',
+})
 
 export default function App({
   Component,
   pageProps: { previewMode, locale, ...pageProps },
 }: AppProps) {
   return (
-    <ReactRuntimeProvider
-      apiOrigin={process.env.MAKESWIFT_API_ORIGIN}
-      runtime={runtime}
-      previewMode={previewMode}
-      locale={locale}
-    >
-      <Component {...pageProps} />
-    </ReactRuntimeProvider>
+    <main className={`${GrenzeGotischFont.variable} ${GrenzeFont.variable}`}>
+      <ReactRuntimeProvider
+        apiOrigin={process.env.MAKESWIFT_API_ORIGIN}
+        runtime={runtime}
+        previewMode={previewMode}
+        locale={locale}
+      >
+        <Component {...pageProps} />
+      </ReactRuntimeProvider>
+    </main>
   )
 }
