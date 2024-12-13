0446cd7f829de90d0469b170c230ad0559c42812
Codeseph <lukemirejoseph@gmail.com>
2024-12-13 11:09:24 -0500

    feat(runtime): add slot component with fallback capability

---
+++ packages/runtime/src/next/components/Slot.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ReactNode, useMemo } from 'react'
+import { MakeswiftComponentSnapshot } from '../client'
+import { MakeswiftComponentType } from '../../components'
+import { MakeswiftComponent } from './MakeswiftComponent'
+import SlotProvider from './SlotProvider'
+
+type Props = {
+  label: string
+  snapshot: MakeswiftComponentSnapshot
+  fallback?: ReactNode
+}
+
+export const Slot = ({ label, snapshot, fallback }: Props) => {
+  const contextValue = useMemo(() => ({ fallback }), [fallback])
+
+  return (
+    <SlotProvider value={contextValue}>
+      <MakeswiftComponent snapshot={snapshot} label={label} type={MakeswiftComponentType.Slot} />
+    </SlotProvider>
+  )
+}
+
+export default Slot
