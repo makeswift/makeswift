19517d837021a345d49fe12c44070a82f8c93571
Arvin Poddar <arvin@makeswift.com>
2025-01-29 14:18:58 -0500

    feat: selectively render head tags in Page
    Introduce prop to the Page component that will selectively render metadata in
    the page `head` with data from Makeswift, or pass booleans to blanket
    enable/disable this data.

--- packages/runtime/src/next/components/page.tsx
+++ packages/runtime/src/next/components/page.tsx
@@ -14,9 +14,11 @@ import {
 } from '../client'
 import * as ReactPage from '../../state/react-page'
 import { registerDocumentsEffect } from '../../state/actions'
+import { type PageMetadataSettings } from '../../components/page/page-seo-settings'
 
 export type PageProps = {
   snapshot: MakeswiftPageSnapshot
+  metadata?: boolean | PageMetadataSettings
 }
 
 function useRegisterPageDocument(pageDocument: MakeswiftPageDocument): ReactPage.Document {
@@ -31,7 +33,25 @@ function useRegisterPageDocument(pageDocument: MakeswiftPageDocument): ReactPage
   return rootDocuments[0]
 }
 
-export const Page = memo(({ snapshot, ...props }: PageProps) => {
+/**
+ * @param snapshot - The snapshot of the page to render, from
+ * `client.getPageSnapshot()`.
+ * @param metadata - Allows control over whether to use data from Makeswift for
+ * rendering metadata tags in the `<head>` of the page. Pass `true` (default if
+ * not provided) to render all metadata tags, or `false` to not render any. For
+ * more granular control, pass an object with boolean values for specific
+ * metadata fields. Valid fields include:
+ *  - `title`
+ *  - `description`
+ *  - `keywords`
+ *  - `socialImage`
+ *  - `canonicalUrl`
+ *  - `indexingBlocked`
+ *  - `favicon`
+ *
+ * If a field is not provided, it will default to `false`.
+ */
+export const Page = memo(({ snapshot, metadata = true, ...props }: PageProps) => {
   if ('runtime' in props) {
     throw new Error(
       `The \`runtime\` prop is no longer supported in the \`@makeswift/runtime\` \`Page\` component as of \`0.15.0\`.
@@ -50,6 +70,7 @@ See our docs for more information on what's changed and instructions to migrate:
         key={snapshot.document.data.key}
         page={snapshot.document}
         rootDocument={rootDocument}
+        metadata={metadata}
       />
     </Suspense>
   )
---
+++ packages/runtime/src/next/components/tests/makeswift-page-metadata-rendering.test.tsx
@@ -0,0 +1,161 @@
+/** @jest-environment jsdom */
+
+import '@testing-library/jest-dom'
+import { act } from 'react-dom/test-utils'
+import { render } from '@testing-library/react'
+import { ReactRuntime } from '../../../react'
+import * as Testing from '../../../runtimes/react/testing'
+import { type MakeswiftPageSnapshot, type MakeswiftPageDocument } from '../../client'
+import { type CacheData } from '../../../api/react'
+import { Page as MakeswiftPage } from '../page'
+import { ComponentPropsWithoutRef } from 'react'
+
+const NoOpComponentType = 'NoOpComponent'
+function NoOpComponent() {
+  return <></>
+}
+
+const pageDocumentFixture = {
+  id: '2222-2222-2222-2222',
+  site: {
+    id: '1111-1111-1111-1111',
+  },
+  data: {
+    type: NoOpComponentType,
+    key: '0000-0000-0000-0000',
+    props: {},
+  },
+  snippets: [],
+  fonts: [],
+  localizedPages: [],
+  locale: null,
+  meta: {
+    title: 'Test Title',
+    description: 'Test Description',
+    keywords: 'test, keywords',
+    socialImage: {
+      id: '0000-0000-0000-0001',
+      publicUrl: 'https://example.com/image.jpg',
+      mimetype: 'image/jpeg',
+    },
+    favicon: {
+      id: '0000-0000-0000-0002',
+      publicUrl: 'https://example.com/favicon.ico',
+      mimetype: 'image/x-icon',
+    },
+  },
+  seo: {
+    canonicalUrl: 'https://example.com',
+    isIndexingBlocked: true,
+  },
+}
+
+function createMakeswiftPageSnapshot(
+  document: MakeswiftPageDocument,
+  cacheData: CacheData = {
+    apiResources: {},
+    localizedResourcesMap: {},
+  },
+): MakeswiftPageSnapshot {
+  return {
+    document,
+    cacheData,
+  }
+}
+
+async function testMakeswiftPageMetadataRendering(
+  props: ComponentPropsWithoutRef<typeof MakeswiftPage>,
+) {
+  const runtime = new ReactRuntime()
+
+  runtime.registerComponent(NoOpComponent, {
+    type: NoOpComponentType,
+    label: 'NoOp Component',
+    props: {},
+  })
+
+  return await act(async () =>
+    render(
+      <Testing.ReactProvider runtime={runtime} previewMode={false}>
+        <MakeswiftPage {...props} />
+      </Testing.ReactProvider>,
+      {
+        container: document.body.appendChild(document.createElement('head')),
+      },
+    ),
+  )
+}
+
+describe('MakeswiftPage', () => {
+  describe('metadata and seo tag rendering', () => {
+    test('renders all metadata by default (without passing prop)', async () => {
+      const snapshot = createMakeswiftPageSnapshot(pageDocumentFixture)
+
+      const render = await testMakeswiftPageMetadataRendering({ snapshot })
+      expect(render.container).toMatchSnapshot()
+    })
+
+    test.each([
+      { label: true, metadata: true },
+      {
+        label: 'all options as true',
+        metadata: {
+          title: true,
+          description: true,
+          keywords: true,
+          socialImage: true,
+          favicon: true,
+          canonicalUrl: true,
+          indexingBlocked: true,
+        },
+      },
+    ])(`renders all metadata when passing $label`, async ({ metadata }) => {
+      const snapshot = createMakeswiftPageSnapshot(pageDocumentFixture)
+
+      const render = await testMakeswiftPageMetadataRendering({ snapshot, metadata })
+      expect(render.container).toMatchSnapshot()
+    })
+
+    test.each([
+      { label: false, metadata: false },
+      {
+        label: 'empty options',
+        metadata: {},
+      },
+      {
+        label: 'all options as false',
+        metadata: {
+          title: false,
+          description: false,
+          keywords: false,
+          socialImage: false,
+          favicon: false,
+          canonicalUrl: false,
+          indexingBlocked: false,
+        },
+      },
+    ])(`does NOT render any metadata when passing $label`, async ({ metadata }) => {
+      const snapshot = createMakeswiftPageSnapshot(pageDocumentFixture)
+
+      const render = await testMakeswiftPageMetadataRendering({ snapshot, metadata })
+      expect(render.container).toMatchSnapshot()
+      expect(render.container.getElementsByTagName('meta').length).toBe(0)
+      expect(render.container.getElementsByTagName('link').length).toBe(0)
+    })
+
+    test('only renders selective metadata when passing options', async () => {
+      const snapshot = createMakeswiftPageSnapshot(pageDocumentFixture)
+
+      // Only render title, description, and keywords
+      const render = await testMakeswiftPageMetadataRendering({
+        snapshot,
+        metadata: {
+          title: true,
+          description: true,
+          keywords: true,
+        },
+      })
+      expect(render.container).toMatchSnapshot()
+    })
+  })
+})
