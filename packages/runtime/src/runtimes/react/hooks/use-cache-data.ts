59f8c6d3d65796905c6966b5de98a2fbf7a6e8a3
Arvin Poddar <arvin@makeswift.com>
2025-02-10 10:25:36 -0500

    fix: prioritize cache hydration over prop resolution
    Fixes an issue where element prop resolution occurs before the cache hydration
    action from the parent is kicked off from an effect. By prioritizing parent
    dispatches ahead of children effects, we ensure that the children are reading
    from hydrated state.

--- packages/runtime/src/runtimes/react/hooks/use-cache-data.ts
+++ packages/runtime/src/runtimes/react/hooks/use-cache-data.ts
@@ -1,10 +1,15 @@
+import { useMemo } from 'react'
 import { type CacheData } from '../../../api/react'
 import { updateAPIClientCache } from '../../../state/actions'
 
 import { useMakeswiftHostApiClient } from '../host-api-client'
-import { useUniversalDispatch } from './use-universal-dispatch'
 
 export function useCacheData(cacheData: CacheData) {
   const { makeswiftApiClient: apiStore } = useMakeswiftHostApiClient()
-  useUniversalDispatch(apiStore.dispatch, updateAPIClientCache, [cacheData])
+
+  // We perform cache hydration immediately on render - this is safe to do per
+  // render because updating the API cache is idempotent. For precedence, see:
+  //
+  // https://github.com/TanStack/query/blob/8f9f183f11df3709a1a38c4efce1452788041f88/packages/react-query/src/HydrationBoundary.tsx#L41
+  useMemo(() => apiStore.dispatch(updateAPIClientCache(cacheData)), [cacheData])
 }
