611a6e85ff33d5b5f7f01c4908054db0516da6a9
jamesqquick <james.q.quick@gmail.com>
2025-03-12 16:51:29 -0500

    fix: update component refeerences to use alias

--- examples/basic-typescript/lib/makeswift/components.ts
+++ examples/basic-typescript/lib/makeswift/components.ts
@@ -1,3 +1,3 @@
-import 'components/Accordions/Accordions.makeswift'
-import 'components/Marquee/Marquee.makeswift'
-import 'components/Tabs/Tabs.makeswift'
+import '@/components/Accordions/Accordions.makeswift'
+import '@/components/Marquee/Marquee.makeswift'
+import '@/components/Tabs/Tabs.makeswift'
