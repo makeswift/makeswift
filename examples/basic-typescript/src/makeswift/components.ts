efaad2f6f17e8e6631064293629757097cfb64f6
jamesqquick <james.q.quick@gmail.com>
2025-03-12 16:49:49 -0500

    fix: update folder structure for typescript basics example

--- examples/basic-typescript/src/makeswift/components.ts
+++
@@ -1,3 +0,0 @@
-import 'src/components/Accordions/Accordions.makeswift'
-import 'src/components/Marquee/Marquee.makeswift'
-import 'src/components/Tabs/Tabs.makeswift'
--- examples/basic-typescript/tailwind.config.js
+++ examples/basic-typescript/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       fontFamily: {
