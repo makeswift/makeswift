c6db2c824d29052d1186a950032ca6bd578e2b07
Codeseph <lukemirejoseph@gmail.com>
2025-03-27 16:31:56 -0400

    fix: document registration occurs prior to prop controller creation for hot reloads

--- packages/runtime/src/runtimes/react/hooks/use-register-document.ts
+++ packages/runtime/src/runtimes/react/hooks/use-register-document.ts
@@ -1,8 +1,9 @@
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect, useMemo } from 'react'
 import { type Document } from '../../../state/react-page'
 import { useDispatch } from './use-dispatch'
 import { useIsInBuilder } from './use-is-in-builder'
 import { registerBuilderDocumentsEffect, registerDocumentsEffect } from '../../../state/actions'
+import { isServer } from '../../../utils/is-server'
 
 /**
  * @param document Document to register
@@ -11,12 +12,24 @@ export function useRegisterDocument(document: Document): void {
   const isInBuilder = useIsInBuilder()
   const dispatch = useDispatch()
 
-  useEffect(() => dispatch(registerDocumentsEffect([document])), [document])
+  const documentArray = useMemo(() => [document], [document])
+
+  if (isServer()) {
+    dispatch(registerDocumentsEffect(documentArray))
+  }
+
+  /*
+    Layout effect is to ensure that the document registration happens prior to the
+    attempted creation/registration of prop controllers in child components.
+  */
+  useLayoutEffect(() => {
+    return dispatch(registerDocumentsEffect(documentArray))
+  }, [dispatch, documentArray])
 
   // TODO: Decide whether to do this via middleware or via explicit action (like
   // what we're doing below)
   useEffect(() => {
     if (!isInBuilder) return
-    return dispatch(registerBuilderDocumentsEffect([document]))
-  }, [isInBuilder, document])
+    return dispatch(registerBuilderDocumentsEffect(documentArray))
+  }, [isInBuilder, documentArray])
 }
