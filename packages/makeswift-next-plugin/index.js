5ed34fcf23fc8053aa55e7a341958fa6eef7c060
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-04-29 19:26:35 -0500

    chore: prettier reformat, no other changes

--- packages/makeswift-next-plugin/index.js
+++ packages/makeswift-next-plugin/index.js
@@ -34,94 +34,94 @@ module.exports =
     appOrigin = 'https://app.makeswift.com',
     previewMode = true,
   } = {}) =>
-  (nextConfig = {}) => {
-    /** @type {NextConfig} */
-    let enhancedConfig = {
-      ...nextConfig,
-      images: {
-        ...nextConfig.images,
-        remotePatterns: [
-          ...(nextConfig.images?.remotePatterns ?? []),
-          ...NEXT_IMAGE_REMOTE_PATTERNS,
-          ...NEXT_IMAGE_REVIEW_APP_REMOTE_PATTERNS,
-        ],
-      },
-      async rewrites() {
-        const rewrites = await nextConfig.rewrites?.()
-        const previewModeRewrites = [
-          {
-            source: '/:path(.*)',
-            has: [
-              {
-                type: 'query',
-                key: 'x-makeswift-draft-mode',
-                value: '(?<secret>.+)',
-              },
-            ],
-            destination: '/api/makeswift/draft',
-          },
-          {
-            source: '/:path(.*)',
-            has: [
-              {
-                type: 'query',
-                key: 'x-makeswift-preview-mode',
-                value: '(?<secret>.+)',
-              },
-            ],
-            destination: '/api/makeswift/preview',
-          },
-        ]
-        return {
-          beforeFiles: [
-            ...(previewMode ? previewModeRewrites : []),
-            ...(Array.isArray(rewrites) ? [] : rewrites?.beforeFiles ?? []),
+    (nextConfig = {}) => {
+      /** @type {NextConfig} */
+      let enhancedConfig = {
+        ...nextConfig,
+        images: {
+          ...nextConfig.images,
+          remotePatterns: [
+            ...(nextConfig.images?.remotePatterns ?? []),
+            ...NEXT_IMAGE_REMOTE_PATTERNS,
+            ...NEXT_IMAGE_REVIEW_APP_REMOTE_PATTERNS,
           ],
-          afterFiles: Array.isArray(rewrites)
-            ? rewrites
-            : rewrites?.afterFiles ?? [],
-          fallback: Array.isArray(rewrites) ? [] : rewrites?.fallback ?? [],
-        }
-      },
-      async headers() {
-        const headers = (await nextConfig.headers?.()) ?? []
-
-        return [
-          ...headers,
-          {
-            source: '/:path*',
-            has: [
-              {
-                type: 'query',
-                key: 'x-makeswift-allow-origin',
-                value: 'true',
-              },
+        },
+        async rewrites() {
+          const rewrites = await nextConfig.rewrites?.()
+          const previewModeRewrites = [
+            {
+              source: '/:path(.*)',
+              has: [
+                {
+                  type: 'query',
+                  key: 'x-makeswift-draft-mode',
+                  value: '(?<secret>.+)',
+                },
+              ],
+              destination: '/api/makeswift/draft',
+            },
+            {
+              source: '/:path(.*)',
+              has: [
+                {
+                  type: 'query',
+                  key: 'x-makeswift-preview-mode',
+                  value: '(?<secret>.+)',
+                },
+              ],
+              destination: '/api/makeswift/preview',
+            },
+          ]
+          return {
+            beforeFiles: [
+              ...(previewMode ? previewModeRewrites : []),
+              ...(Array.isArray(rewrites) ? [] : rewrites?.beforeFiles ?? []),
             ],
-            headers: [
-              {
-                key: 'Access-Control-Allow-Origin',
-                value: appOrigin,
-              },
-            ],
-          },
-        ]
-      },
+            afterFiles: Array.isArray(rewrites)
+              ? rewrites
+              : rewrites?.afterFiles ?? [],
+            fallback: Array.isArray(rewrites) ? [] : rewrites?.fallback ?? [],
+          }
+        },
+        async headers() {
+          const headers = (await nextConfig.headers?.()) ?? []
+
+          return [
+            ...headers,
+            {
+              source: '/:path*',
+              has: [
+                {
+                  type: 'query',
+                  key: 'x-makeswift-allow-origin',
+                  value: 'true',
+                },
+              ],
+              headers: [
+                {
+                  key: 'Access-Control-Allow-Origin',
+                  value: appOrigin,
+                },
+              ],
+            },
+          ]
+        },
+      }
+
+      const nextVersion = require('next/package.json').version
+
+      if (satisfies(nextVersion, '<13.4.0')) {
+        throw new Error('Makeswift requires a minimum Next.js version of 13.4.0.')
+      }
+
+      return {
+        ...enhancedConfig,
+        webpack(config, options) {
+          config = enhancedConfig.webpack?.(config, options) ?? config
+
+          if (resolveSymlinks != null) config.resolve.symlinks = resolveSymlinks
+
+          return config
+        },
+      }
     }
-
-    const nextVersion = require('next/package.json').version
-
-    if (satisfies(nextVersion, '<13.4.0')) {
-      throw new Error('Makeswift requires a minimum Next.js version of 13.4.0.')
-    }
-
-    return {
-      ...enhancedConfig,
-      webpack(config, options) {
-        config = enhancedConfig.webpack?.(config, options) ?? config
-
-        if (resolveSymlinks != null) config.resolve.symlinks = resolveSymlinks
-
-        return config
-      },
-    }
-  }
