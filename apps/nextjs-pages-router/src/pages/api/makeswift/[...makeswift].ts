a34b706dca04cbedf5658ad54ce7504b1dd474ea
Fikri Karim <fkfikrikarim@gmail.com>
2025-05-02 21:02:47 +0700

    chore: update the component registration for apps/pages-router

--- apps/nextjs-pages-router/src/pages/api/makeswift/[...makeswift].ts
+++ apps/nextjs-pages-router/src/pages/api/makeswift/[...makeswift].ts
@@ -2,12 +2,6 @@ import { MAKESWIFT_SITE_API_KEY } from '@/makeswift/env'
 import { runtime } from '@/makeswift/runtime'
 import { MakeswiftApiHandler } from '@makeswift/runtime/next/server'
 
-// This import is required for the Smartling integration.
-// To translate a page, we need to iterate the component data finding all translatable strings.
-// Without control definitions, we can't traverse the component data.
-// Importing components here allows us to get translatable strings and merge them back once they are translated.
-import '@/makeswift/components'
-
 export default MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY, {
   runtime,
   apiOrigin: process.env.MAKESWIFT_API_ORIGIN,
