7dd44f639ac1f5e41b34ed4eab2911cef5396fb9
Fikri Karim <fkfikrikarim@gmail.com>
2025-05-15 13:44:21 +0700

    remove welcome

--- apps/remix/app/routes/home.tsx
+++ apps/remix/app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <p>Test</p>
 }
