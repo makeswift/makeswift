d59013a066b70d0cca81071d2bcdcd4669dbb09e
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-05-20 23:49:56 -0500

    fix: `Cannot use import statement outside a module` on Emotion modules

--- apps/remix/vite.config.ts
+++ apps/remix/vite.config.ts
@@ -4,5 +4,18 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  ssr: {
+    noExternal: [
+      '@emotion/cache',
+      '@emotion/css',
+      '@emotion/hash',
+      '@emotion/memoize',
+      '@emotion/serialize',
+      '@emotion/sheet',
+      '@emotion/unitless',
+      '@emotion/utils',
+      '@emotion/weak-memoize',
+    ],
+  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
