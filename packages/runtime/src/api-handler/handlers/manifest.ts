635ce7b497e64bb667a4efe4b430abe22af2cf14
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-05-29 20:57:05 -0500

    refactor: framework-independent API handler implementation

---
+++ packages/runtime/src/api-handler/handlers/manifest.ts
@@ -0,0 +1,48 @@
+import {
+  type ApiRequest,
+  type ErrorResponseBody,
+  ApiResponse,
+  searchParams,
+} from '../request-response'
+
+export type Manifest = {
+  version: string
+  previewMode: boolean
+  draftMode: boolean
+  interactionMode: boolean
+  clientSideNavigation: boolean
+  elementFromPoint: boolean
+  customBreakpoints: boolean
+  siteVersions: boolean
+  unstable_siteVersions: boolean
+  localizedPageSSR: boolean
+  webhook: boolean
+  localizedPagesOnlineByDefault: boolean
+}
+
+export async function manifestHandler(
+  req: ApiRequest,
+  { apiKey, manifest }: { apiKey: string; manifest: Partial<Manifest> },
+): Promise<ApiResponse<Manifest | ErrorResponseBody>> {
+  const secret = searchParams(req).get('secret')
+
+  if (secret !== apiKey) {
+    return ApiResponse.json({ message: 'Unauthorized' }, { status: 401 })
+  }
+
+  return ApiResponse.json({
+    version: PACKAGE_VERSION,
+    previewMode: false,
+    draftMode: false,
+    interactionMode: true,
+    clientSideNavigation: false,
+    elementFromPoint: false,
+    customBreakpoints: true,
+    siteVersions: true,
+    unstable_siteVersions: true,
+    localizedPageSSR: true,
+    webhook: true,
+    localizedPagesOnlineByDefault: true,
+    ...manifest,
+  })
+}
--- packages/runtime/src/next/api-handler/draft.ts
+++ packages/runtime/src/next/api-handler/draft.ts
@@ -2,17 +2,8 @@ export const PRERENDER_BYPASS_COOKIE = '__prerender_bypass'
 export const PREVIEW_DATA_COOKIE = '__next_preview_data'
 
 export const MAKESWIFT_DRAFT_DATA_COOKIE = 'x-makeswift-draft-data'
-export const SET_COOKIE_HEADER = 'set-cookie'
 
 export const SearchParams = {
   DraftMode: 'x-makeswift-draft-mode',
   PreviewMode: 'x-makeswift-preview-mode',
-} as const
-
-export const cookieSettingOptions = {
-  path: '/',
-  sameSite: 'none',
-  secure: true,
-  httpOnly: true,
-  partitioned: true,
 } as const
