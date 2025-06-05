47521a740238ece896e06f72edfe94f1bd347f39
tyler coonin <ty@makeswift.com>
2025-06-05 17:10:09 -0400

    spike: added a link and custom image markdown

--- apps/nextjs-app-router/src/components/Box/Box.makeswift.ts
+++ apps/nextjs-app-router/src/components/Box/Box.makeswift.ts
@@ -11,16 +11,14 @@ runtime.registerComponent(Box, {
   hidden: false,
   description: `
   
-**This is an awesome image:**
+  
+*This is an awesome image:*
 
-<AwesomeImageComponent src="https://plus.unsplash.com/premium_photo-1738614647383-0435fcb26a55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D" />
-<br></br>
-<AwesomeImageComponent src="https://images.unsplash.com/photo-1596276567596-8eb1b5994cfb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZG9nJTIwbWVtZXxlbnwwfHwwfHx8MA%3D%3D" />
-<br></br>
-<button onClick={() => alert('This is a test script from MDX!')}>
+![robot](https://plus.unsplash.com/premium_photo-1738614647383-0435fcb26a55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D)
+<AwesomeImageComponent src="https://images.unsplash.com/photo-1596276567596-8eb1b5994cfb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZG9nJTIwbWVtZXxlbnwwfHwwfHx8MA%3D%3D" alt = "dog pizza"/>
+<button>
   Suspicious text...
 </button>
-<br></br>
 [Important Docs](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
 `,
   props: {
