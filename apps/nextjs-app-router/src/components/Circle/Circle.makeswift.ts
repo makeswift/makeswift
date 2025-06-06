ad538a45d5426cf38bf9b1411849341f3a8e3816
tyler coonin <ty@makeswift.com>
2025-06-06 18:06:24 -0400

    spike: added more content to finished MDX spike

--- apps/nextjs-app-router/src/components/Circle/Circle.makeswift.ts
+++ apps/nextjs-app-router/src/components/Circle/Circle.makeswift.ts
@@ -2,16 +2,17 @@ import { Style } from "@makeswift/runtime/controls";
 
 import { runtime } from "@/makeswift/runtime";
 
-import { Box } from "./Box";
+import { Circle } from "./Circle";
 
-runtime.registerComponent(Box, {
-  type: 'MegaTestBox',
-  label: 'Custom / TestBox',
-  icon: 'cube',
+runtime.registerComponent(Circle, {
+  type: 'SuperCircleTester',
+  label: 'Custom / TestCircle',
+  icon: 'chats',
   hidden: false,
   description: `
-  
-  
+This is awesome text
+<h1>This is header</h1>
+
 *This is an awesome image:*
 
 ![robot](https://plus.unsplash.com/premium_photo-1738614647383-0435fcb26a55?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D)
@@ -20,6 +21,7 @@ runtime.registerComponent(Box, {
   Suspicious text...
 </button>
 [Important Docs](https://www.youtube.com/watch?v=dQw4w9WgXcQ)
+<CustomLink link="https://www.youtube.com/watch?v=dQw4w9WgXcQ" other="Click me!" />
 `,
   props: {
     className: Style({ properties: Style.All }),
--- apps/nextjs-app-router/src/makeswift/components.ts
+++ apps/nextjs-app-router/src/makeswift/components.ts
@@ -1,3 +1,4 @@
 import '@/components/font-control-demo/font-control-demo.makeswift'
 import '@/components/group-demo/group-demo.makeswift'
 import "@/components/Box/Box.makeswift";
+import "@/components/Circle/Circle.makeswift";
