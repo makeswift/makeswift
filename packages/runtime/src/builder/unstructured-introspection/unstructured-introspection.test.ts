48d65379d5b6d31e9e5b8990e34760b32784b9e1
Codeseph <lukemirejoseph@gmail.com>
2025-06-10 20:01:25 -0400

    chore(runtime): update unstructured introspection regex to handle colons in node ID values

--- packages/runtime/src/builder/unstructured-introspection/unstructured-introspection.test.ts
+++ packages/runtime/src/builder/unstructured-introspection/unstructured-introspection.test.ts
@@ -8,4 +8,20 @@ describe('Unstructured Introspection', () => {
     )
     expect(introspectedResourceIds).toMatchSnapshot('Resources relevant to cross-site copy/paste')
   })
+  test('recognizes node ID that has colons in its value', () => {
+    const typeName = "Swatch"
+    const value = "a:b::c"
+    const nodeId = Buffer.from(`${typeName}:${value}`).toString('base64')
+    const element = {
+      "key": "00000000-0000-0000-0000-000000000000",
+      "props": {
+        "testProp": {
+          "swatchId": nodeId,
+        },
+      },
+      "type": "test"
+    }
+    const introspectedResourceIds = unstructuredIntrospection(element)
+    expect(Array.from(introspectedResourceIds.swatchIds)).toEqual([nodeId])
+  })
 })
