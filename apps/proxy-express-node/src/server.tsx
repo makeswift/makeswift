89e7493ec8b1c861e9af505cb99322a1f928145e
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-06-16 21:12:58 -0500

    proxy-express-node: allow to override the port

--- apps/proxy-express-node/src/server.tsx
+++ apps/proxy-express-node/src/server.tsx
@@ -94,7 +94,7 @@ app.get('*', async (req, res) => {
   }
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Proxy server running at http://localhost:${PORT}`)
