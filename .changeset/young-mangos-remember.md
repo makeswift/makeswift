8b9fd2ef18898acb0b1677f6dad9d7da87c4d053
Arvin Poddar <arvin@makeswift.com>
2025-07-10 13:13:29 -0400

    fix: support preflight requests in app router
    Update the API handler to support OPTIONS requests

---
+++ .changeset/young-mangos-remember.md
@@ -0,0 +1,5 @@
+---
+"@makeswift/runtime": patch
+---
+
+Update API handler to correctly support preflight `OPTIONS` requests for app router hosts
--- apps/nextjs-app-router/src/app/api/makeswift/[...makeswift]/route.ts
+++ apps/nextjs-app-router/src/app/api/makeswift/[...makeswift]/route.ts
@@ -56,4 +56,4 @@ const handler = MakeswiftApiHandler(MAKESWIFT_SITE_API_KEY, {
   },
 })
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST, handler as OPTIONS }
