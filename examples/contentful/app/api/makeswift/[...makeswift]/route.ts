f845ec83e097a0fcd498e9ace3d505e3a1c9dce8
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-07-13 15:37:31 -0400

    fix: add OPTIONS request hanlding to all of the App Router examples

--- examples/contentful/app/api/makeswift/[...makeswift]/route.ts
+++ examples/contentful/app/api/makeswift/[...makeswift]/route.ts
@@ -23,4 +23,4 @@ const handler = MakeswiftApiHandler(env.MAKESWIFT_SITE_API_KEY, {
   },
 })
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST, handler as OPTIONS }
