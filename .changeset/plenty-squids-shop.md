60ef737ce4de0404676f36cc6c131669396fa40f
Arvin Poddar <arvin@makeswift.com>
2025-08-16 18:57:55 -0400

    feat: add query param to clear preview cookies

---
+++ .changeset/plenty-squids-shop.md
@@ -0,0 +1,6 @@
+---
+"@makeswift/next-plugin": patch
+"@makeswift/runtime": patch
+---
+
+feat: add query param to clear preview cookies on redirect
--- packages/runtime/src/runtimes/react/components/preview-switcher/preview-toolbar.tsx
+++ packages/runtime/src/runtimes/react/components/preview-switcher/preview-toolbar.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react'
+
 const makeswiftIcon =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAACXBIWXMAABcRAAAXEQHKJvM/AAABjElEQVRYhc2XzU3EMBCFB8TddAAXn6EE6GCpgNABZ1/IXnymBOgAOmA7YM8+ABVsXEHQQFaKQryeN3Yk3ilKJtEnv/nLUd/3pFG0riGi88yrnQn+UfJ5FUi0riWiB2H4nQn+KRd0DFP8agXEfkqCYJBoHdtxIQxfm+DfFgEhoith3NYE30o/qgGR2BJB+xY7kdYEL8oNFUi0jiFMJuxVWrJqEMFxsyUNCsE6AeNztvBp7aJ143vXksoRnwhYtmNdSoIQa6RlO9YXEWW7KgoCleOgxgTf1QZBT+RZ2lXFING6UxCCq+ceeUE8fYdknY599v9sJvzGBP+yCEgC7GPmETc0OJ+0awAlkhe2pAbIXAeFZ8xe2g2Nk3c3ub0xwWt6zY9qbmiqGVMbZK21ZC/YmhlbeBMTzZNDQqcvDb1kM1x32iqZSt1HaqukfKvq34BAOTLsrH+ETNmUkKHHA+428RgeclPVWozeSyAI2EdWB34jtqXNTAySOY3i/KgFIlqOa4GkFmBegorzg4joG07he/M7zl6jAAAAAElFTkSuQmCC'
 
@@ -19,6 +21,7 @@ const styles = `
     0 11px 40px 0 rgba(0, 0, 0, 0.25),
     0 2px 10px 0 rgba(0, 0, 0, 0.12);
   transition: 150ms;
+  text-decoration: none;
 
   font-family: system-ui, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji',
     'Segoe UI Emoji';
@@ -42,19 +45,22 @@ const styles = `
 }
 `
 
-type Props = {
-  onExitPreview: () => void
-}
+export function PreviewToolbar() {
+  const redirectLiveUrl = useMemo(() => {
+    const currentUrl = new URL(window.location.href)
+    const destination = `${currentUrl.pathname}${currentUrl.search}${currentUrl.hash}`
+    currentUrl.searchParams.set('x-makeswift-redirect-live', encodeURIComponent(destination))
+    return currentUrl.toString()
+  }, [])
 
-export function PreviewToolbar({ onExitPreview: onExitDraft }: Props) {
   return (
     <>
       <style dangerouslySetInnerHTML={{ __html: styles }} />
       <div className="exit-preview-container">
-        <button className="exit-preview-button" onClick={onExitDraft}>
+        <a className="exit-preview-button" href={redirectLiveUrl}>
           <img src={makeswiftIcon} alt="Makeswift Logo" width={18} height={18} />
           <span className="label">Exit preview</span>
-        </button>
+        </a>
       </div>
     </>
   )
