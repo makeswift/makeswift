0ac45a3137c2e8cf13be4ad4fc28810b2e2af6ba
Arvin Poddar <arvin@makeswift.com>
2025-08-18 17:08:06 -0400

    chore: upgrade runtime version for basic examples

--- examples/basic-typescript-pages/pages/api/makeswift/[...makeswift].ts
+++ examples/basic-typescript-pages/pages/api/makeswift/[...makeswift].ts
@@ -2,6 +2,8 @@ import { runtime } from "@/lib/makeswift/runtime";
 import { MakeswiftApiHandler } from "@makeswift/runtime/next/server";
 import { strict } from "assert";
 
+import '@/lib/makeswift/components'
+
 strict(
   process.env.MAKESWIFT_SITE_API_KEY,
   "MAKESWIFT_SITE_API_KEY is required"
