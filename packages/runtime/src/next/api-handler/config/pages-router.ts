5ef70cad9285f740e6962d19b0c537f45341d117
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-08-21 15:27:51 -0500

    refactor: generalize Node <-> Fetch API request/response conversion

--- packages/runtime/src/next/api-handler/config/pages-router.ts
+++ packages/runtime/src/next/api-handler/config/pages-router.ts
@@ -2,6 +2,7 @@ import { P } from 'ts-pattern'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 import { type ApiResponse } from '../../../api-handler/request-response'
+import { toApiRequest, pipeResponseTo } from '../../../api-handler/node-request-response'
 
 import { pagesRouterRedirectPreviewHandler } from '../handlers/pages-router-redirect-preview'
 import { PRERENDER_BYPASS_COOKIE, PREVIEW_DATA_COOKIE } from '../preview'
@@ -22,30 +23,13 @@ export async function config({
   client: MakeswiftClient
 }): Promise<ApiHandlerConfig> {
   return {
-    req: {
-      headers: requestHeaders(req.headers),
-      method: req.method ?? 'GET',
-      url: req.url ?? '',
-      json() {
-        return req.body
-      },
-    },
+    req: toApiRequest(req),
     route: validateApiRoute(await apiRequestParams(req)),
     previewCookieNames: [PRERENDER_BYPASS_COOKIE, PREVIEW_DATA_COOKIE],
-    sendResponse: async (apiResponse: ApiResponse): Promise<Response | void> => {
-      const headers = responseHeaders(apiResponse.headers)
-      Object.entries(headers).forEach(([key, value]) => {
-        res.setHeader(key, value)
-      })
 
-      res.status(apiResponse.status)
+    sendResponse: (apiResponse: ApiResponse): Promise<Response | void> =>
+      pipeResponseTo(apiResponse, res),
 
-      if (apiResponse.body) {
-        await pipeTo(apiResponse.body, res)
-      }
-
-      res.end()
-    },
     revalidationHandler: async (path?: string): Promise<void> => {
       if (path != null) {
         res.revalidate(path)
@@ -53,6 +37,7 @@ export async function config({
         // No-op, Pages Router does not support tag-based revalidation
       }
     },
+
     customRoutes: async (route: string) => {
       if (route === '/redirect-preview') {
         return { res: await pagesRouterRedirectPreviewHandler(req, res, client) }
@@ -67,48 +52,4 @@ function apiRequestParams(request: NextApiRequest): Promise<NextApiRequest['quer
   // `NextApiRequest.query` prop became async in Next.js 15, but it's not reflected in the type definition;
   // force-casting it to a `Promise` manually
   return Promise.resolve(request.query)
-}
-
-function requestHeaders(headers: NextApiRequest['headers']): Headers {
-  const result = new Headers()
-  for (const [key, value] of Object.entries(headers)) {
-    if (value != null) {
-      if (Array.isArray(value)) {
-        value.forEach(v => result.append(key, v))
-      } else {
-        result.append(key, value)
-      }
-    }
-  }
-
-  return result
-}
-
-function responseHeaders(headers: Headers): Record<string, string | string[]> {
-  return [...headers.entries()].reduce<Record<string, string | string[]>>((acc, [key, value]) => {
-    if (key in acc) {
-      const existingValue = acc[key]
-      if (Array.isArray(existingValue)) {
-        existingValue.push(value)
-      } else {
-        acc[key] = [existingValue, value]
-      }
-    } else {
-      acc[key] = value
-    }
-    return acc
-  }, {})
-}
-
-async function pipeTo(stream: ReadableStream<Uint8Array>, res: NextApiResponse): Promise<void> {
-  try {
-    const reader = stream.getReader()
-    while (true) {
-      const { done, value } = await reader.read()
-      if (done) break
-      res.write(value)
-    }
-  } catch (error) {
-    res.destroy(error instanceof Error ? error : new Error(`${error}`))
-  }
 }
