c2e7fe2de668808fb1415914aaee24291498077f
Arvin Poddar <arvin@makeswift.com>
2025-08-21 23:40:24 -0400

    fix: api route handler type compatibility
    In Next.js 15.5.0, API handler types are now validated at build time. Our
    current handler type was not matching with Next.js's expected definition,
    resulting in build errors. This fix modifies our API handler return type to
    match the expected types by Next.

--- packages/runtime/src/next/api-handler/index.ts
+++ packages/runtime/src/next/api-handler/index.ts
@@ -17,10 +17,10 @@ type ApiHandlerArgs = AppRouter.ApiHandlerArgs | PagesRouter.ApiHandlerArgs
 export function MakeswiftApiHandler(
   apiKey: string,
   { apiOrigin, runtime, ...userConfig }: ApiHandlerUserConfig,
-): (...args: ApiHandlerArgs) => Promise<Response | void> {
+): (...args: ApiHandlerArgs) => Promise<Response> | Promise<void> {
   const client = new MakeswiftClient(apiKey, { apiOrigin, runtime })
 
-  return async function handler(...args: ApiHandlerArgs): Promise<Response | void> {
+  async function handler(...args: ApiHandlerArgs): Promise<Response | void> {
     const { req, route, sendResponse, customRoutes, ...handlerConfig } = await match(args)
       .with(AppRouter.argsPattern, ([req, context]) => AppRouter.config({ req, context, client }))
       .with(PagesRouter.argsPattern, ([req, res]) => PagesRouter.config({ req, res, client }))
@@ -40,4 +40,18 @@ export function MakeswiftApiHandler(
 
     return sendResponse(await apiHandler(req, route))
   }
+
+  // Next 15.5.0 performs type validations on the API handler. It expects a
+  // return type of void | Promise<void> | Response | Promise<Response>, but
+  // Typescript will not allow you to define an async function that returns a
+  // union of two different Promises:
+
+  // The return type of an async function or method must be the global
+  // Promise<T> type. Did you mean to write 'Promise<void | Response>'?
+
+  // This cast converts our handler's Promise<Response | void> return type to
+  // Promise<Response> | Promise<void> to satisfy Next.js validations and bypass
+  // the Typescript restriction. Ultimately, the value of the resolved type is
+  // the same (Response | void).
+  return handler as (...args: ApiHandlerArgs) => Promise<Response> | Promise<void>
 }
