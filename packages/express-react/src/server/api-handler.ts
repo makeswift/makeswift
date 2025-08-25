d08783f00ccdb1e82ca60b51c03347e9e8172739
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-08-24 23:47:16 -0500

    feat: alpha `express-react` package

---
+++ packages/express-react/src/server/api-handler.ts
@@ -0,0 +1,47 @@
+import {
+  type Request as ExpressRequest,
+  type Response as ExpressResponse,
+  type RequestHandler,
+  type NextFunction,
+} from 'express'
+
+import {
+  type ApiHandlerUserConfig,
+  MAKESWIFT_SITE_VERSION_COOKIE,
+  createApiHandler as createMakeswiftApiHandler,
+  toApiRequest,
+  pipeResponseTo,
+} from '@makeswift/runtime/unstable-framework-support'
+
+import { Makeswift as MakeswiftClient } from '../client'
+
+const API_PATH_PREFIX = '/api/makeswift/'
+
+type UserConfig = ApiHandlerUserConfig & {
+  revalidationHandler?: (path?: string) => Promise<void>
+}
+
+export function createApiHandler(
+  apiKey: string,
+  { apiOrigin, runtime, revalidationHandler, ...userConfig }: UserConfig,
+): RequestHandler {
+  const client = new MakeswiftClient(apiKey, { apiOrigin, runtime })
+
+  return async function apiHandler(req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
+    if (!req.path.startsWith(API_PATH_PREFIX)) {
+      return next()
+    }
+
+    const route = `/${req.path.replace(API_PATH_PREFIX, '')}`
+
+    const apiHandler = createMakeswiftApiHandler(apiKey, {
+      ...userConfig,
+      previewCookieNames: [MAKESWIFT_SITE_VERSION_COOKIE],
+      revalidationHandler: revalidationHandler ?? (() => Promise.resolve()),
+      runtime,
+      client,
+    })
+
+    return pipeResponseTo(await apiHandler(toApiRequest(req), route), res)
+  }
+}
---
+++ packages/express-react/src/server/preview.ts
@@ -0,0 +1,67 @@
+import {
+  type Request as ExpressRequest,
+  type Response as ExpressResponse,
+  type RequestHandler,
+  type NextFunction,
+} from 'express'
+
+import {
+  type SiteVersion,
+  MAKESWIFT_SITE_VERSION_COOKIE,
+  REDIRECT_SEARCH_PARAM,
+  cookieSettingOptions,
+  MakeswiftClient,
+  pipeResponseTo,
+  redirectLiveHandler,
+  SearchParams,
+  secondsUntilSiteVersionExpiration,
+  serializeSiteVersion,
+  toApiRequest,
+} from '@makeswift/runtime/unstable-framework-support'
+
+async function requestedSiteVersion({
+  searchParams,
+  client,
+}: {
+  searchParams: URLSearchParams
+  client: MakeswiftClient
+}): Promise<SiteVersion | null> {
+  const previewToken = searchParams.get(SearchParams.PreviewToken)
+  if (previewToken == null) return null
+
+  const verifiedToken = await client.readPreviewToken(previewToken)
+  if (verifiedToken == null) return null
+
+  const { payload } = verifiedToken
+
+  return { version: payload.version, token: previewToken }
+}
+
+export function createPreviewMiddleware({ client }: { client: MakeswiftClient }): RequestHandler {
+  return async function apiHandler(req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
+    const { pathname, searchParams } = new URL(req.url, `https://example.com`)
+
+    const redirectLive = searchParams.get(REDIRECT_SEARCH_PARAM)
+    if (redirectLive != null) {
+      return pipeResponseTo(
+        await redirectLiveHandler(toApiRequest(req), {
+          previewCookieNames: [MAKESWIFT_SITE_VERSION_COOKIE],
+        }),
+        res,
+      )
+    }
+
+    const siteVersion = await requestedSiteVersion({ searchParams, client })
+    if (siteVersion == null) return next()
+
+    const serializedSiteVersion = serializeSiteVersion(siteVersion)
+    const secondsUntilExpiration = secondsUntilSiteVersionExpiration(siteVersion)
+
+    res.cookie(MAKESWIFT_SITE_VERSION_COOKIE, serializedSiteVersion, {
+      ...cookieSettingOptions,
+      maxAge: secondsUntilExpiration * 1000, // Express expects maxAge in milliseconds
+    })
+
+    return res.redirect(pathname)
+  }
+}
