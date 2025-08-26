187d0befbd36347a3bebd8aaf2ae4fd2adfc6bfc
Fikri Karim <fkfikrikarim@gmail.com>
2025-08-26 16:31:21 +0700

    rendering all widget templates

--- apps/widget-transformer/src/components/transformer/index.tsx
+++ apps/widget-transformer/src/components/transformer/index.tsx
@@ -1,32 +1,36 @@
 import { renderTemplate } from '@/lib/template-renderer'
 import { createWidgetContext } from '@/lib/schema-transformer'
-import buttonWidgetTemplate from '@/lib/button-widget-template.json'
 import { PropsContextProvider } from './client'
 import { MakeswiftComponent } from '@makeswift/runtime/next'
-import { ButtonKind } from './register'
 import { client } from '@/makeswift/client'
 import { getSiteVersion } from '@makeswift/runtime/next/server'
 import { resolveProps } from '@/makeswift/prop-resolution'
+import WidgetTemplates from '@/lib/widget-templates.json'
 
-interface TransformerProps {
-  [key: string]: any
+type Props = {
+  widgetTemplate: (typeof WidgetTemplates)[number]
 }
 
-export async function Transformer(_props: TransformerProps = {}) {
-  const snapshot = await client.getComponentSnapshot('button', {
+export async function Transformer({ widgetTemplate }: Props) {
+  const snapshot = await client.getComponentSnapshot(widgetTemplate.kind, {
     siteVersion: getSiteVersion(),
   })
   // @ts-expect-error
-  const props = resolveProps(snapshot.document.data.props)
-  const context = createWidgetContext('button', props, buttonWidgetTemplate)
-  const html = await renderTemplate(buttonWidgetTemplate.template, context)
+  const makeswiftProps = resolveProps(snapshot.document.data?.props ?? {})
+  const context = createWidgetContext(
+    widgetTemplate.kind,
+    makeswiftProps,
+    // @ts-expect-error
+    widgetTemplate,
+  )
+  const html = await renderTemplate(widgetTemplate.template, context)
 
   return (
     <PropsContextProvider value={{ html }}>
       <MakeswiftComponent
-        label={'Transformer'}
+        label={widgetTemplate.name}
         snapshot={snapshot}
-        type={ButtonKind}
+        type={widgetTemplate.kind}
       />
     </PropsContextProvider>
   )
