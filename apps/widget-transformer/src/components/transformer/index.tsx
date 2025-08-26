f5786bab9d27785082221532aeef3149fc718075
Fikri Karim <fkfikrikarim@gmail.com>
2025-08-26 15:53:47 +0700

    use createWidgetContext

--- apps/widget-transformer/src/components/transformer/index.tsx
+++ apps/widget-transformer/src/components/transformer/index.tsx
@@ -1,13 +1,20 @@
-import { renderTemplate, createDefaultContext } from '@/lib/template-renderer'
+import { renderTemplate } from '@/lib/template-renderer'
+import { createWidgetContext } from '@/lib/schema-transformer'
 import buttonWidgetTemplate from '@/lib/button-widget-template.json'
 import { PropsContextProvider } from './client'
 import { MakeswiftComponent } from '@makeswift/runtime/next'
 import { generateEmptyComponentSnapshot } from '@/makeswift/utils'
 import { ButtonKind } from './register'
 
-export async function Transformer() {
+interface TransformerProps {
+  [key: string]: any
+}
+
+export async function Transformer(props: TransformerProps = {}) {
+  // Generate context automatically from widget schema and props
+  const context = createWidgetContext('button-widget-123', props, buttonWidgetTemplate)
+  
   // Render the template on the server with BigCommerce helpers
-  const context = createDefaultContext('button-widget-123')
   const html = await renderTemplate(buttonWidgetTemplate.template, context)
 
   return (
