746ca57efaa57c61cbf917936785caa0b99eeca5
Fikri Karim <fkfikrikarim@gmail.com>
2025-08-26 18:29:39 +0700

    split createWidgetContext

--- apps/widget-transformer/src/components/transformer/index.tsx
+++ apps/widget-transformer/src/components/transformer/index.tsx
@@ -1,17 +1,20 @@
 import { renderTemplate } from '@/lib/template-renderer'
-import { createWidgetContext } from '@/lib/schema-transformer'
 import { PropsContextProvider } from './client'
 import { MakeswiftComponent } from '@makeswift/runtime/next'
 import { client } from '@/makeswift/client'
 import { getSiteVersion } from '@makeswift/runtime/next/server'
 import { resolveProps } from '@/makeswift/prop-resolution'
 import WidgetTemplates from '@/lib/widget-templates.json'
+import { createWidgetContext } from '@/lib/widget-context'
 
 type Props = {
   widgetTemplate: (typeof WidgetTemplates)[number]
 }
 
 export async function Transformer({ widgetTemplate }: Props) {
+  // Debugging sd-product
+  if (widgetTemplate.kind !== 'sd-product') return <></>
+
   const snapshot = await client.getComponentSnapshot(widgetTemplate.kind, {
     siteVersion: getSiteVersion(),
   })
