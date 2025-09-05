makeswift/makeswift

commit ba2e9decf564c108673c30a5aabcfcf1863ff5b1

chore: API handler type compatibility follow-up

packages/runtime/src/next/api-handler/config/app-router.ts
@@ -14,18 +14,15 @@ import { MakeswiftClient } from '../../../client'
 type Params = { [key: string]: string | string[] }
 type Context = { params: Promise<Params> }
 
-export type ApiHandlerArgs = [NextRequest | Request, Context]
-export const argsPattern = [
-  P.union(P.instanceOf(Request), P.instanceOf(NextRequest)),
-  P.any,
-] as const
+export type ApiHandlerArgs = [NextRequest, Context]
+export const argsPattern = [P.instanceOf(NextRequest), P.any] as const
 
 export async function config({
   req,
   context,
   client,
 }: {
-  req: NextRequest | Request
+  req: NextRequest
   context: Context
   client: MakeswiftClient
 }): Promise<ApiHandlerConfig> {
@@ -43,12 +40,7 @@ export async function config({
     },
     customRoutes: async (route: string) => {
       if (route === '/redirect-preview') {
-        // Convert any Request to NextRequest, if necessary. Due to an issue in
-        // how NextRequests are instantiated, we have to pass the original
-        // request to the constructor twice. See
-        // https://github.com/vercel/next.js/issues/52967
-        const request = req instanceof NextRequest ? req : new NextRequest(req, req)
-        return { res: await appRouterRedirectPreviewHandler(request, context, client) }
+        return { res: await appRouterRedirectPreviewHandler(req, context, client) }
       }
 
       return null

commit 872ec781e1d2f16b3d66041a377a93565cffa5e7

fix: match Request and NextRequest in API handler
Fixes a regression in ba2e9de where the API handler does not match vanilla
Requests. In older versions of Next (prior to 15.5.0), the inbound request is a
Request. In newer versions, the request is a NextRequest. To maintain
compatibility with all Next.js versions, we match against both.

packages/runtime/src/next/api-handler/config/app-router.ts
@@ -14,15 +14,23 @@ import { MakeswiftClient } from '../../../client'
 type Params = { [key: string]: string | string[] }
 type Context = { params: Promise<Params> }
 
-export type ApiHandlerArgs = [NextRequest, Context]
-export const argsPattern = [P.instanceOf(NextRequest), P.any] as const
+// In older versions of Next (prior to 15.5.0), the inbound request is typed as
+// a `NextRequest`, but requests received at runtime fail the `instanceof
+// NextRequest` check. In newer versions of Next, requests are correctly typed
+// as `NextRequest`. To maintain compatibility across all Next.js versions, we
+// match against both.
+export type ApiHandlerArgs = [NextRequest | Request, Context]
+export const argsPattern = [
+  P.union(P.instanceOf(Request), P.instanceOf(NextRequest)),
+  P.any,
+] as const
 
 export async function config({
   req,
   context,
   client,
 }: {
-  req: NextRequest
+  req: NextRequest | Request
   context: Context
   client: MakeswiftClient
 }): Promise<ApiHandlerConfig> {
@@ -40,7 +48,12 @@ export async function config({
     },
     customRoutes: async (route: string) => {
       if (route === '/redirect-preview') {
-        return { res: await appRouterRedirectPreviewHandler(req, context, client) }
+        // Convert any Request to NextRequest, if necessary. Due to an issue in
+        // how NextRequests are instantiated, we have to pass the original
+        // request to the constructor twice. See
+        // https://github.com/vercel/next.js/issues/52967
+        const request = req instanceof NextRequest ? req : new NextRequest(req, req)
+        return { res: await appRouterRedirectPreviewHandler(request, context, client) }
       }
 
       return null