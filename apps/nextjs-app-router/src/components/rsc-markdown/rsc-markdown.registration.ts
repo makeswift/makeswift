268dab476c734ca45da8f2bb63d09a550beae9b5
Fikri Karim <fkfikrikarim@gmail.com>
2025-09-12 19:50:58 +0700

    Separate client and server components registration

--- apps/nextjs-app-router/src/components/rsc-markdown/rsc-markdown.registration.ts
+++ apps/nextjs-app-router/src/components/rsc-markdown/rsc-markdown.registration.ts
@@ -1,12 +1,11 @@
-import { runtime } from '@/makeswift/runtime'
-import { RscMarkdown } from './rsc-markdown'
 import { Style, TextInput } from '@makeswift/runtime/controls'
 
-runtime.registerComponent(RscMarkdown, {
+export const RscMarkdownRegistration = {
   type: 'rsc-markdown',
   label: 'Custom / RSC Markdown',
   props: {
     className: Style(),
     filename: TextInput({ label: 'Filename' }),
   },
-})
+  server: true,
+}
--- apps/nextjs-app-router/src/makeswift/components.ts
+++
@@ -1,3 +0,0 @@
-import '@/components/font-control-demo/font-control-demo.makeswift'
-import '@/components/group-demo/group-demo.makeswift'
-import '@/components/rsc-markdown/rsc-markdown.makeswift'
