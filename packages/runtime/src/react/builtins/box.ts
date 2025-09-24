69b27ad962cbd453a5dbe3bf850c63a74fd15240
Arvin Poddar <arvin@makeswift.com>
2025-09-24 16:27:15 -0400

    feat: expose bare runtime and registrations
    Export the core `ReactRuntime` without any registrations, along with functions
    to register any of the builtin components. Each component has its own entrypoint
    for proper treeshaking.

---
+++ packages/runtime/src/react/builtins/box.ts
@@ -0,0 +1,2 @@
+export { registerComponent as registerBoxComponent } from '../../components/builtin/Box/register'
+export { default as Box } from '../../components/builtin/Box/Box'
---
+++ packages/runtime/src/react/builtins/navigation.ts
@@ -0,0 +1,2 @@
+export { registerComponent as registerNavigationComponent } from '../../components/builtin/Navigation/register'
+export { default as Navigation } from '../../components/builtin/Navigation/Navigation'
