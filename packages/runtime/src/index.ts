064408f038ba076e15ef28d237b92cf97c2ea888
Arvin Poddar <arvin@makeswift.com>
2025-09-26 17:52:26 -0400

    chore: remove export for internal operation type

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,4 +1,3 @@
-export type { Operation } from './state/modules/read-write-documents'
 export type { ComponentMeta } from './state/modules/components-meta'
 export { ComponentIcon } from './state/modules/components-meta'
 export { MakeswiftComponentType } from './components/builtin/constants'
