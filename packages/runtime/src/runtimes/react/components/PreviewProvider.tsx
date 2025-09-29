bd0161a960b7b30411e23afa77e73733f35f0902
Fikri Karim <fkfikrikarim@gmail.com>
2025-09-29 20:54:18 +0700

    feat: add ElementData and previewStoreMiddlewares to FrameworkProvider
    We need to replace ElementData for RSC and add a middleware to preview store

--- packages/runtime/src/runtimes/react/components/PreviewProvider.tsx
+++ packages/runtime/src/runtimes/react/components/PreviewProvider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { type PropsWithChildren, useEffect, useMemo } from 'react'
+import { type Middleware } from '@reduxjs/toolkit'
 
 import * as ReactBuilderPreview from '../../../state/react-builder-preview'
 
@@ -8,16 +9,27 @@ import { useReactRuntime } from '../hooks/use-react-runtime'
 import { StoreContext } from '../hooks/use-store'
 import { useMakeswiftHostApiClient } from '../host-api-client'
 
-export default function PreviewProvider({ children }: PropsWithChildren): JSX.Element {
+export default function PreviewProvider({
+  children,
+  middlewares = [],
+}: PropsWithChildren<{
+  middlewares?: Middleware<
+    ReactBuilderPreview.Dispatch,
+    ReactBuilderPreview.State,
+    ReactBuilderPreview.Dispatch
+  >[]
+}>): JSX.Element {
   const runtime = useReactRuntime()
   const client = useMakeswiftHostApiClient()
+
   const store = useMemo(
     () =>
       ReactBuilderPreview.configureStore({
         preloadedState: runtime.store.getState(),
         client,
+        middlewares,
       }),
-    [client, runtime],
+    [client, runtime, middlewares],
   )
 
   useEffect(() => {
