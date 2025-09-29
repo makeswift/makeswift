552ec6a432aee5df1dc6c355236d21f31fdad640
Fikri Karim <fkfikrikarim@gmail.com>
2025-09-29 20:59:00 +0700

    feat: add experimental RSC support

---
+++ packages/runtime/src/next/rsc/index.ts
@@ -0,0 +1,2 @@
+export { NextRSCRuntime as ExperimentalReactRuntime } from './shared/react-runtime'
+export { NextRSCRuntimeProvider as ExperimentalReactRuntimeProvider } from './client/runtime-provider'
