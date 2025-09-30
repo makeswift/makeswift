28fa9e4c1b4d9f75c1b52f529dda10f651732b9a
Drew Pledger <drew@makeswift.com>
2025-09-30 12:56:23 -0400

    example: Contentstack simple

---
+++ examples/contentstack-simple/components/warning/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Warning = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="rounded-lg border border-yellow-200 bg-yellow-50 p-4">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <svg
+            className="h-5 w-5 text-yellow-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.178 2.625-1.516 2.625H3.72c-1.337 0-2.189-1.458-1.515-2.625L8.485 2.495zM10 5a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 5zm0 9a1 1 0 100-2 1 1 0 000 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <div className="ml-3">
+          <p className="text-sm text-yellow-800">{children}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
