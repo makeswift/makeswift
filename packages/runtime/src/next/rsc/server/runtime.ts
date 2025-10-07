9775619c0e0599a3fb04d868fea7fc69ef289215
Arvin Poddar <arvin@makeswift.com>
2025-10-07 14:03:30 -0400

    fix: pass site version via cache
    Avoid always rendering RSCBuilderUpdater by passing the site version via cache.

--- packages/runtime/src/next/rsc/server/runtime.ts
+++ packages/runtime/src/next/rsc/server/runtime.ts
@@ -2,8 +2,12 @@ import 'server-only'
 
 import { cache } from 'react'
 import { ReactRuntime } from '../../../runtimes/react/react-runtime'
+import { SiteVersion } from '../../../unstable-framework-support'
 
 const getRuntimeCache = cache((): { runtime: ReactRuntime | null } => ({ runtime: null }))
+const getSiteVersionCache = cache((): { siteVersion: SiteVersion | null } => ({
+  siteVersion: null,
+}))
 
 export function setRuntime(runtime: ReactRuntime): void {
   const cache = getRuntimeCache()
@@ -17,4 +21,14 @@ export const getRuntime = (): ReactRuntime => {
     throw new Error('Makeswift context not found. Ensure your page uses `runWithMakeswiftContext`.')
   }
   return cache.runtime
+}
+
+export function setSiteVersion(siteVersion: SiteVersion | null): void {
+  const cache = getSiteVersionCache()
+  cache.siteVersion = siteVersion
+}
+
+export function getSiteVersionFromCache(): SiteVersion | null {
+  const cache = getSiteVersionCache()
+  return cache.siteVersion
 }
