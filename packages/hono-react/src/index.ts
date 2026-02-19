dacd878f9dd1635709fc47ab0f3d8e09041651a5
Codeseph <lukemirejoseph@gmail.com>
2026-02-19 14:35:08 -0500

    chore: reexport experimental GoogleFontLink from hono-react

--- packages/hono-react/src/index.ts
+++ packages/hono-react/src/index.ts
@@ -4,6 +4,7 @@ export {
   Page,
   Slot,
   RuntimeProvider as ReactRuntimeProvider,
+  GoogleFontLink as Unstable_GoogleFontLink,
 } from '@makeswift/runtime/unstable-framework-support'
 
 export { Makeswift } from './client'
