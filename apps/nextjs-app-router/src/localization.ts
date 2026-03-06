3272e8b72e20c513cfb8c0bd068a5ea5ac971d7a
Aleksey Gurtovoy <agurtovoy@acm.org>
2026-03-06 14:55:07 -0600

    testing(nextjs-app-router): add a hardcoded route, extra locale

--- apps/nextjs-app-router/src/localization.ts
+++ apps/nextjs-app-router/src/localization.ts
@@ -2,9 +2,9 @@ import { NextRequest } from 'next/server'
 import { match } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
-export type Locale = 'en-US' | 'fr-FR'
+export type Locale = 'en-US' | 'fr-FR' | 'es-MX'
 export let defaultLocale: Locale = 'en-US'
-export let locales: Locale[] = ['en-US', 'fr-FR']
+export let locales: Locale[] = ['en-US', 'fr-FR', 'es-MX']
 
 export function getLocale(request: NextRequest): Locale {
   const languages = new Negotiator({
