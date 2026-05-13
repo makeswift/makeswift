6b0fc71c26e49bdc18be8bd46bb8e72850c799f0
Aleksey Gurtovoy <agurtovoy@acm.org>
2026-05-13 00:01:18 -0500

    fix(runtime): move current breakpoint calculation to the root
    this reduces the number of `window.matchMedia` calls/event handlers,
    which significantly improves rendering performance on Safari

--- packages/runtime/src/runtimes/react/testing/react-runtime.tsx
+++ packages/runtime/src/runtimes/react/testing/react-runtime.tsx
@@ -1,6 +1,7 @@
+import { type BreakpointsInput } from '../../../state/modules/breakpoints'
 import { TestOrigins } from '../../../testing/fixtures'
 import { ReactRuntime } from '../react-runtime'
 
-export function createReactRuntime() {
-  return new ReactRuntime({ fetch, ...TestOrigins })
+export function createReactRuntime({ breakpoints }: { breakpoints?: BreakpointsInput } = {}) {
+  return new ReactRuntime({ fetch, breakpoints, ...TestOrigins })
 }
