77edc83614b40781e1dee45898345fe3108e4204
Codeseph <lukemirejoseph@gmail.com>
2026-05-26 14:18:36 -0400

    update test component for demoing style v2

--- apps/next-rsc/src/makeswift/components/client-style-v2-test/client-style-v2-test.makeswift.ts
+++ apps/next-rsc/src/makeswift/components/client-style-v2-test/client-style-v2-test.makeswift.ts
@@ -1,7 +1,6 @@
 import { runtime } from '@/makeswift/runtime'
 import { Checkbox, unstable_StyleV2, TextInput } from '@makeswift/runtime/controls'
 import { ClientStyleV2Test } from './client-style-v2-test'
-import { CSSObject } from '@emotion/serialize'
 
 runtime.registerComponent(ClientStyleV2Test, {
   type: 'client-style-v2-test',
@@ -17,10 +16,29 @@ runtime.registerComponent(ClientStyleV2Test, {
   },
 })
 
+/*
+  Whether the style object below uses:
+
+  '&:hover'
+  or
+  ':hover'
+
+  is useful for demoing a behavioral difference
+  between Emotion and what we have right now. Emotion
+  (via a Stylis plugin used by its cache) would assume
+  the intent of the developer was to make the ':hover'
+  apply to the parent, and based on this assumption would
+  append a '&' before ':hover' during css preprocessing.
+
+  If we don't make that assumption, then the result of omitting
+  the '&' below would be that the hover effect applies to descendants
+  rather than to the parent (a descendant selector gets created)
+
+*/
 function getHoverStyle(useFunHover: boolean) {
   return {
     transition: 'background 0.15s ease-in-out',
-    ':hover': {
+    '&:hover': {
       background: useFunHover ? 'purple' : 'white',
     },
   }
