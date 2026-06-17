e9e0ce1610d0b25f46f4208cf3081f50b85a7d56
Codeseph <lukemirejoseph@gmail.com>
2026-06-17 13:18:08 -0400

    (NOT to be included yet) update packages/express-react for css runtime changes...

--- packages/express-react/src/server/render.tsx
+++ packages/express-react/src/server/render.tsx
@@ -4,24 +4,19 @@ import { Writable } from 'stream'
 import { type ReactNode } from 'react'
 
 import {
-  createRootStyleCache,
   RootStyleRegistry,
-  styleTagHtml,
   type RootStyleProps,
 } from '@makeswift/runtime/unstable-framework-support'
 
 export function renderHtml(
   children: ReactNode,
-  { cacheKey, enableCssReset }: RootStyleProps = {},
-): Promise<{ getStyles: () => string; html: string }> {
-  const cache = createRootStyleCache({ key: cacheKey })
-  const getStyles = () => styleTagHtml({ cacheKey: cache.key, ...cache.flush() })
-
+  { classNamePrefix, enableCssReset }: RootStyleProps = {},
+): Promise<{ html: string }> {
   return new Promise((resolve, reject) => {
     let html = ''
 
     const { pipe } = renderToPipeableStream(
-      <RootStyleRegistry cache={cache} enableCssReset={enableCssReset}>
+      <RootStyleRegistry classNamePrefix={classNamePrefix} enableCssReset={enableCssReset}>
         {children}
       </RootStyleRegistry>,
       {
@@ -33,7 +28,7 @@ export function renderHtml(
                 cb()
               },
               final() {
-                resolve({ html, getStyles })
+                resolve({ html })
               },
             }),
           )
