410ebc42132f4f208c0b46eb2287d4e15a15e07f
Drew Cutchins <drew.cutchins@bigcommerce.com>
2026-08-06 12:21:00 -0400

    chore(runtime): Add tests for mount change buffering

---
+++ packages/runtime/src/state/middleware/read-write/builder-api/__tests__/index.test.ts
@@ -0,0 +1,62 @@
+/** @jest-environment jsdom */
+import { configureStore as configureReduxStore } from '@reduxjs/toolkit'
+
+import { middlewareOptions } from '../../../../toolkit'
+import { createRootReducer } from '../../../../read-write-state'
+import { handleWheel, mountComponent, unmountComponent } from '../../../../builder-api/actions'
+import { type BuilderAPIProxy } from '../../../../builder-api/proxy'
+
+import { builderAPIMiddleware } from '../index'
+
+describe('builderAPIMiddleware', () => {
+  const mockExecute = jest.fn()
+  const builderProxy = { execute: mockExecute } as unknown as BuilderAPIProxy
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const createStore = () =>
+    configureReduxStore({
+      reducer: createRootReducer(),
+      middleware: getDefaultMiddleware =>
+        getDefaultMiddleware(middlewareOptions).concat(builderAPIMiddleware(builderProxy)),
+    })
+
+  describe('handling builder actions', () => {
+    it('immediately executes non mount change actions', () => {
+      const store = createStore()
+      const action = handleWheel({ deltaX: 1, deltaY: 2 })
+
+      store.dispatch(action)
+
+      expect(mockExecute).toHaveBeenCalledWith(action)
+    })
+
+    it('executes mount component actions after the current task completes', async () => {
+      const store = createStore()
+      const action = mountComponent('documentKey', 'elementKey')
+
+      store.dispatch(action)
+
+      expect(mockExecute).not.toHaveBeenCalled()
+
+      await Promise.resolve()
+
+      expect(mockExecute).toHaveBeenCalledWith(action)
+    })
+
+    it('executes unmount component actions after the current task completes', async () => {
+      const store = createStore()
+      const action = unmountComponent('documentKey', 'elementKey')
+
+      store.dispatch(action)
+
+      expect(mockExecute).not.toHaveBeenCalled()
+
+      await Promise.resolve()
+
+      expect(mockExecute).toHaveBeenCalledWith(action)
+    })
+  })
+})
---
+++ packages/runtime/src/state/middleware/read-write/builder-api/__tests__/mount-change-action-buffer.test.ts
@@ -0,0 +1,42 @@
+import { mountComponent, unmountComponent } from '../../../../builder-api/actions'
+import { MountChangeActionBuffer } from '../mount-change-action-buffer'
+
+const mockExecute = jest.fn()
+
+describe('MountChangeActionBuffer', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('executes scheduled actions after the current task completes', async () => {
+    const buffer = new MountChangeActionBuffer(mockExecute)
+
+    const action = mountComponent('a', '1')
+    buffer.schedule(action)
+
+    expect(mockExecute).not.toHaveBeenCalled()
+
+    await Promise.resolve()
+
+    expect(mockExecute).toHaveBeenCalledWith(action)
+  })
+
+  it('removes rundundant actions before execution', async () => {
+    const buffer = new MountChangeActionBuffer(mockExecute)
+
+    const actions = [
+      mountComponent('a', '1'),
+      unmountComponent('a', '1'),
+      mountComponent('a', '1'),
+      mountComponent('a', '2'),
+    ]
+
+    actions.forEach(action => buffer.schedule(action))
+
+    await Promise.resolve()
+
+    expect(mockExecute).toHaveBeenCalledTimes(2)
+    expect(mockExecute).toHaveBeenCalledWith(mountComponent('a', '1'))
+    expect(mockExecute).toHaveBeenCalledWith(mountComponent('a', '2'))
+  })
+})
