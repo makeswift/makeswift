c224a4b61f6fc23d37315a01093fe05e7b7a9c7f
Codeseph <lukemirejoseph@gmail.com>
2026-08-21 20:13:57 -0400

    chore: update vite-rsc-hono-react example app with changes for the new css runtime

--- apps/vite-rsc-hono-react-cloudflare/src/framework/render-context.ts
+++ apps/vite-rsc-hono-react-cloudflare/src/framework/render-context.ts
@@ -3,6 +3,7 @@ import { type Context } from 'hono'
 import { getSiteVersion } from '@makeswift/hono-react/server'
 
 import { type HonoEnv } from '../lib/hono'
+import { rootStyleProps } from '../lib/makeswift/styles'
 
 export const createRenderContext = async (
   c: Context<HonoEnv>,
@@ -17,6 +18,7 @@ export const createRenderContext = async (
     client: c.var.makeswiftClient,
     siteVersion,
     locale,
+    rootStyleProps,
     store: runtime.getOrCreateStore({ siteVersion, locale }),
   }
 }
