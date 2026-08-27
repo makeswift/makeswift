1e2a6f5e83fc2ca65b12b5bc06b3458a7e8d7280
Aleksey Gurtovoy <agurtovoy@acm.org>
2026-08-27 17:01:52 -0500

    refactor(runtime): pass config instead of full rich text definition
    Pass config instead of full rich text definition when rendering rich
    text

--- packages/runtime/src/runtimes/react/controls/rich-text-v2/render-rich-text-v2.tsx
+++ packages/runtime/src/runtimes/react/controls/rich-text-v2/render-rich-text-v2.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode, lazy } from 'react'
 
-import { type DataType } from '@makeswift/controls'
+import { type DataType, type ConfigType } from '@makeswift/controls'
 
 import { RichTextV2Control, RichTextV2Definition } from '../../../../controls/rich-text-v2'
 
@@ -10,12 +10,12 @@ const ReadOnlyTextV1 = lazy(() => import('../rich-text/ReadOnlyText'))
 
 export function renderRichTextV2(
   data: DataType<RichTextV2Definition> | undefined,
-  definition: RichTextV2Definition,
+  config: ConfigType<RichTextV2Definition>,
   control: RichTextV2Control | null,
 ): ReactNode {
   return RichTextV2Definition.isV1Data(data) ? (
     <ReadOnlyTextV1 text={data} />
   ) : (
-    <RichTextV2Value data={data} definition={definition} control={control} />
+    <RichTextV2Value data={data} config={config} control={control} />
   )
 }
