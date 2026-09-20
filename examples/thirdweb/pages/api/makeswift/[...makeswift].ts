28b8072c857ecd85afe85902b7e1e78f9f2e60a1
Arvin Poddar <arvin@makeswift.com>
2026-09-20 11:19:43 -0400

    chore: remove thirdweb example

--- examples/thirdweb/pages/api/makeswift/[...makeswift].ts
+++
@@ -1,5 +0,0 @@
-import { MakeswiftApiHandler } from '@makeswift/runtime/next/server'
-import { getConfig } from 'lib/config'
-
-const config = getConfig()
-export default MakeswiftApiHandler(config.makeswiftSiteApiKey)
