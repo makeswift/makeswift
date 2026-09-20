42de4ebe8461ba65329c5b83e74df1d18cd06d0a
Arvin Poddar <arvin@makeswift.com>
2026-09-20 11:17:41 -0400

    chore: remove bigcommerce example app

--- examples/bigcommerce/pages/api/categories.ts
+++
@@ -1,7 +0,0 @@
-import { getCategories } from 'lib/bigcommerce'
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const categories = await getCategories()
-  res.status(200).json(categories)
-}
