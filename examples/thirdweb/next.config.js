5e756cf509e18e662f6bce516a4639a8e8b5a0b3
Arvin Poddar <arvin@makeswift.com>
2026-09-20 11:19:43 -0400

    chore: remove thirdweb example

--- examples/thirdweb/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const withMakeswift = require('@makeswift/runtime/next/plugin')()
-
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = withMakeswift(nextConfig)
