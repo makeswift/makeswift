06a024dd32f686a18b1a4d9d6b6d9a0e4c6d7516
Arvin Poddar <arvin@makeswift.com>
2026-09-20 11:18:22 -0400

    chore: remove bigcommerce-catalyst app

--- examples/bigcommerce-catalyst/components/product-form/fields/shared/error-message.tsx
+++
@@ -1,3 +0,0 @@
-export const ErrorMessage = ({ children }: { children: React.ReactNode }) => (
-  <p className="mt-2 text-xs font-normal text-red-100">{children}</p>
-);
--- examples/bigcommerce-catalyst/components/ui/navigation-menu/index.ts
+++
@@ -1,3 +0,0 @@
-'use client';
-
-export * from './navigation-menu';
